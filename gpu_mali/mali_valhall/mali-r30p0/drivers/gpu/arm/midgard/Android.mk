a54c5e9e8017bb3bc3566925925dea2f2870fd87
mtk15701 <chien-lun.hsu@mediatek.com>
2021-05-13 11:41:29 +0800

    [ALPS05663750] GPU: port DDK to out of tree
    [Detail]
    - Port DDK to out of tree
    - Add files:
      Android.mk: set config for each platform
      gpu_mali/mali_valhall/mali-r30p0/
        drivers/gpu/arm/midgard/Android.mk: build ko
      gpu_mali/mali_valhall/mali-r30p0/
        drivers/gpu/arm/midgard/init.mali_kbase.rc: load ko
    
    MTK-Commit-Id: ded59a6cc50c2c6dd78b4f2c461d21fb56e59aba
    
    Change-Id: Id400d878c0d14a35a0c2fe4c05457dab62f58b3f
    CR-Id: ALPS05663750
    Feature: GPU ARM(gpu_arm)
    Signed-off-by: mtk15701 <chien-lun.hsu@mediatek.com>

---
+++ gpu_mali/mali_valhall/mali-r30p0/drivers/gpu/arm/midgard/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mali_kbase_$(LOCAL_MTK_PLATFORM).ko
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_INIT_RC := init.mali_kbase.rc
+include $(MTK_KERNEL_MODULE)
+
+GPU_OPTS := MTK_GPU_VERSION=$(LOCAL_MTK_GPU_VERSION) MTK_PLATFORM=$(LOCAL_MTK_PLATFORM)
+$(info [GPU][DDK] GPU_OPTS = $(GPU_OPTS))
+$(linked_module): OPTS += $(GPU_OPTS)
