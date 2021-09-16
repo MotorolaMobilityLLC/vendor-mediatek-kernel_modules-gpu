e2be0e14ad9e967376100a6b88741755a03fffbe
Hans Ke <hans.ke@mediatek.com>
2021-09-16 12:59:06 +0800

    [ALPS05707932] GPU: enable GPU BM(GPU Qos) on mt6879
    [Detail]
    enable GPU BM(GPU Qos) on mt6879
    
    MTK-Commit-Id: 186776fd579d79676ecb6b8a29c9c173dc9a4009
    
    CR-Id: ALPS05707932
    Feature: GPU ARM(gpu_arm)
    Change-Id: I2d0efcf5df5618dabfdac5c6a26e03542315ead9
    Signed-off-by: Hans Ke <hans.ke@mediatek.com>

--- Android.mk
+++ Android.mk
@@ -58,6 +58,7 @@ GPU_OPTS += CONFIG_MALI_MIDGARD_DVFS=y
 GPU_OPTS += CONFIG_MALI_DEVFREQ=y
 GPU_OPTS += CONFIG_MALI_MTK_DEVFREQ=y
 GPU_OPTS += CONFIG_MALI_MTK_DEVFREQ_THERMAL=y
+GPU_OPTS += CONFIG_MALI_MTK_GPU_BM_2=y
 include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/gpu/arm/midgard/Android.mk
 
 LOCAL_MTK_GPU_VERSION := mali_valhall_r32p1
