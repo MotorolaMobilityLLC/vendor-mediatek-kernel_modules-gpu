dddcd572fb7deda0e4180177d528911c79b32434
Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
2022-01-21 18:45:17 +0800

    [ALPS06537790] Valhall-r32p1: Add debugging dumps fp for Mali fence
    [Detail]
    * Print debug info for active GPU command queue groups
    * Print per-context KCPU queues debug information
    * Print per-context KCPU queues debug information
    
    MTK-Commit-Id: f7052bac35df4ab00f0f42f30acc983a8c71a449
    
    Signed-off-by: Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
    CR-Id: ALPS06537790
    Feature: ARM GPU DDK driver
    Change-Id: I032f591ec633349aba8ae025e534046c7e735745
    (cherry picked from commit 23cff6ae4a738055774adbf53984d3fc9ad15bcf)

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
@@ -7,5 +7,8 @@
 #define __MTK_PLATFORM_DEBUG_H__
 
 void mtk_common_debug_dump_status(void);
+void mtk_common_gpu_fence_debug_dump(int fd, int pid, int type);
+
+extern void (*mtk_gpu_fence_debug_dump_fp)(int fd, int pid, int type);
 
 #endif /* __MTK_PLATFORM_DEBUG_H__ */
