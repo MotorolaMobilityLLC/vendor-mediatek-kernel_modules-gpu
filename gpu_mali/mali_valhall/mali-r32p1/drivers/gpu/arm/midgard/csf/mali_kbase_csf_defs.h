022adbee8fb1ed77ab0e9bb9630acc589f6996a5
mtk15651 <bocheng.chen@mediatek.com>
2022-02-17 15:38:08 +0800

    [ALPS06263764] GPU: Reduce gpu idle time
    For low power saving, reduce idle time to quick gpu power on/off
    
    MTK-Commit-Id: d07063016bacfbf2fd13747f92cea0dd827386ff
    
    CR-Id: ALPS06263764
    Feature: GPU ARM(gpu_arm)
    Change-Id: I2f09b577edf31f58296ebe6ddbf59938ff03cc2c
    Signed-off-by: mtk15651 <bocheng.chen@mediatek.com>

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_defs.h
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_defs.h
@@ -892,7 +892,7 @@ struct kbase_csf_scheduler {
 /**
  * Default GLB_PWROFF_TIMER_TIMEOUT value in unit of micro-seconds.
  */
-#define DEFAULT_GLB_PWROFF_TIMEOUT_US (800)
+#define DEFAULT_GLB_PWROFF_TIMEOUT_US (300)
 
 /**
  * In typical operations, the management of the shader core power transitions
