6bfb17a07ba991d37b51f75317b668e50df55bd5
Qoo Lin <qoo.lin@mediatek.com>
2022-02-17 15:38:35 +0800

    [ALPS06107308] GPU : support kernel build flow
    Build out-of-tree kernel module by kernel build flow
    Including the following three modules
    - mali_kbase_mtXXXX.ko
    - mali_mgm.ko
    - mali_prot_alloc.ko
    
    MTK-Commit-Id: 2810a8b8eba09b28126a335e0d07ded9d71df3fa
    
    Signed-off-by: Qoo Lin <qoo.lin@mediatek.com>
    CR-Id: ALPS06107308
    Feature: GPU ARM(gpu_arm)
    Change-Id: I502a9885ad883822fc7a29550311d2e63bb1cc06

--- gpu_mali/mali_valhall/mali-r32p1/drivers/base/arm/protected_memory_allocator/Android.mk
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/base/arm/protected_memory_allocator/Android.mk
@@ -6,6 +6,6 @@ LOCAL_PROPRIETARY_MODULE := true
 LOCAL_INIT_RC := init.mali_prot_alloc.rc
 include $(MTK_KERNEL_MODULE)
 
- $(info [GPU][DDK] GPU_OPTS = $(GPU_OPTS))
+$(info [GPU][DDK] GPU_OPTS = $(GPU_OPTS))
 $(linked_module): OPTS += $(GPU_OPTS)
 
