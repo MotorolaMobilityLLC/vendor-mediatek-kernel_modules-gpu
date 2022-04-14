2b67a354bc31aaf02337665993bc079f183468f6
Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
2022-04-14 17:07:08 +0800

    [ALPS07093653] Valhall-r32p1: Apply arm patch 3986
    [Detail]
    GPUCORE-33613: Prevent CSG slot reuse until GPU reset after CSG term timeout
    
    => https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/3986
    
    MTK-Commit-Id: c5014ba305657c1ff001847c56e42f47cf66ab0f
    
    CR-Id: ALPS07093653
    Feature: ARM GPU DDK driver
    Change-Id: I43c25748cca8a431f33fa1dffb3a3fd4015253fc

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_reset_gpu.c
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_reset_gpu.c
@@ -593,6 +593,14 @@ bool kbase_reset_gpu_is_active(struct kbase_device *kbdev)
 	return kbase_csf_reset_state_is_active(reset_state);
 }
 
+bool kbase_reset_gpu_is_not_pending(struct kbase_device *kbdev)
+{
+	enum kbase_csf_reset_gpu_state reset_state =
+		atomic_read(&kbdev->csf.reset.state);
+
+	return (reset_state != KBASE_CSF_RESET_GPU_NOT_PENDING);
+}
+
 int kbase_reset_gpu_wait(struct kbase_device *kbdev)
 {
 	const long wait_timeout =
