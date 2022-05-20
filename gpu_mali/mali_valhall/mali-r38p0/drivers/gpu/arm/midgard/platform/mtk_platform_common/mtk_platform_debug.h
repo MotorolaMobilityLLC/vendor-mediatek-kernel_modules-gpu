3f7781889c6f8b6b2e9eb9bafc3d94f388e1538a
mtk16965 <holiday.cheng@mediatek.com>
2022-05-20 13:06:08 +0800

    [ALPS07095598] r38p0: mt6985: sleep mode porting
    [Sleep Mode Enable Condition]
    1. Only products starting from Turse support sleep mode
    2. Need to program the Runtime PM related operation in
       Kbase platform porting
    
    [Porting]
    1. hook the runtime PM init/terminate callback
    2. hook the runtime gpu acitve/idle callback
    3. use the config - CONFIG_MALI_SLEEP_MODE in Makefile
       to control sleep on/off
    4. use KBASE_PLATFORM_SUSPEND_DELAY in platform code
       to change autosuspend delay
    5. use KBASE_PLATFORM_DEBUG_ENABLE in platform code
       to control debug log enable
    
    MTK-Commit-Id: 5eb776404311f471ba88373fb0c10700cf7769a2
    
    CR-Id: ALPS07095598
    Feature: ARM GPU DDK driver
    Change-Id: I0f69966215123d90995179dbde06ebef1c572668

--- gpu_mali/mali_valhall/mali-r38p0/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
+++ gpu_mali/mali_valhall/mali-r38p0/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.h
@@ -6,6 +6,18 @@
 #ifndef __MTK_PLATFORM_DEBUG_H__
 #define __MTK_PLATFORM_DEBUG_H__
 
+#define KBASE_PLATFORM_TAG  "[KBASE/PLATFORM]"
+#define KBASE_PLATFORM_LOGD(fmt, args...) \
+	do { if (KBASE_PLATFORM_DEBUG_ENABLE) \
+            {pr_info(KBASE_PLATFORM_TAG"[DEBUG]@%s: "fmt"\n", __func__, ##args);} \
+        else \
+            {pr_debug(KBASE_PLATFORM_TAG"[DEBUG]@%s: "fmt"\n", __func__, ##args);} \
+        } while (0)
+#define KBASE_PLATFORM_LOGE(fmt, args...) \
+	pr_info(KBASE_PLATFORM_TAG"[ERROR]@%s: "fmt"\n", __func__, ##args)
+#define KBASE_PLATFORM_LOGI(fmt, args...) \
+	pr_info(KBASE_PLATFORM_TAG"[INFO]@%s: "fmt"\n", __func__, ##args)
+
 void mtk_common_debug_dump_pm_status(struct kbase_device *kbdev);
 void mtk_debug_csf_dump_groups_and_queues(struct kbase_device *kbdev, int pid);
 void mtk_debug_dump_for_external_fence(int fd, int pid, int type, int timeouts);
