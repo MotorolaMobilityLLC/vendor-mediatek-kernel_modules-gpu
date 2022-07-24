e9b697ed33b572d585f435188b91d696c7ae3901
mtk16965 <holiday.cheng@mediatek.com>
2022-07-24 12:56:08 +0800

    [ALPS07431541] r38p1: common: debugfs node for sleep mode
    [Detail]
    Add the debugfs node for checking sleep mode enable
    status.
    Use the following command to check:
    cat /sys/kernel/debug/mali0/sleep_mode
    
    MTK-Commit-Id: 75b04a1edd09d5423857e0a5aedadbf1981b24ac
    
    CR-Id: ALPS07431541
    Feature: ARM GPU DDK driver
    Change-Id: Iae76c54c740682dceebef1b6e6268ebefe8cdaa3

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
@@ -310,10 +310,47 @@ void mtk_common_sysfs_term(struct kbase_device *kbdev)
 #endif /* CONFIG_MALI_MTK_SYSFS */
 
 #if IS_ENABLED(CONFIG_MALI_MTK_DEBUG_FS)
+static int mtk_debug_sleep_mode(struct seq_file *file, void *data)
+{
+#if IS_ENABLED(CONFIG_MALI_SLEEP_MODE)
+	seq_printf(file, "Sleep mode: Enabled\n");
+#else /* CONFIG_MALI_SLEEP_MODE */
+	seq_printf(file, "Sleep mode: Disabled\n");
+#endif /* CONFIG_MALI_SLEEP_MODE */
+	return 0;
+}
+
+static int mtk_sleep_mode_debugfs_open(struct inode *in, struct file *file)
+{
+	return single_open(file, mtk_debug_sleep_mode,
+	                   in->i_private);
+}
+
+static const struct file_operations mtk_sleep_mode_debugfs_fops = {
+	.open = mtk_sleep_mode_debugfs_open,
+	.read    = seq_read,
+	.llseek  = seq_lseek,
+	.release = single_release
+};
+
+int mtk_debug_sleep_mode_debugfs_init(struct kbase_device *kbdev)
+{
+	if (IS_ERR_OR_NULL(kbdev))
+		return -1;
+
+	debugfs_create_file("sleep_mode", 0440,
+		kbdev->mali_debugfs_directory, kbdev,
+		&mtk_sleep_mode_debugfs_fops);
+
+	return 0;
+}
+
 void mtk_common_debugfs_init(struct kbase_device *kbdev)
 {
 	if (IS_ERR_OR_NULL(kbdev))
 		return;
+
+	mtk_debug_sleep_mode_debugfs_init(kbdev);
 }
 
 #if IS_ENABLED(CONFIG_MALI_CSF_SUPPORT)
