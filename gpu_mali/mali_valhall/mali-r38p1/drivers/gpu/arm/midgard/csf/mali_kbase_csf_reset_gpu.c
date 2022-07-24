45dbd6f7ec0026212bc3a60438e77bbc01d02b1d
GuanWen Chen <gw.chen@mediatek.com>
2022-07-24 12:55:44 +0800

    [ALPS07431650] revert trace_buffer_mutex_r38p0.patch
    [Detail]
    ARM Ticket: 00446853
    
    r38p1 do not need this patch
    revert it
    
    MTK-Commit-Id: e1a838192177fa6d89ded71d233c40899489e1bf
    
    Signed-off-by: GuanWen Chen <gw.chen@mediatek.com>
    CR-Id: ALPS07431650
    Feature: ARM GPU DDK driver
    Change-Id: I7c60bfa24509a092299892f6b37826eb6a80c05c

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_reset_gpu.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_reset_gpu.c
@@ -272,19 +272,16 @@ static void kbase_csf_dump_firmware_trace_buffer(struct kbase_device *kbdev)
 	unsigned int read_size, remaining_size;
 	struct firmware_trace_buffer *tb;
 
-	mutex_lock(&kbdev->trace_buffer_mutex);
 	tb = kbase_csf_firmware_get_trace_buffer(kbdev, FW_TRACE_BUF_NAME);
 
 	if (tb == NULL) {
 		dev_vdbg(kbdev->dev, "Can't get the trace buffer, firmware trace dump skipped");
-		mutex_unlock(&kbdev->trace_buffer_mutex);
 		return;
 	}
 
 	buf = kmalloc(PAGE_SIZE + 1, GFP_KERNEL);
 	if (buf == NULL) {
 		dev_err(kbdev->dev, "Short of memory, firmware trace dump skipped");
-		mutex_unlock(&kbdev->trace_buffer_mutex);
 		return;
 	}
 
@@ -328,7 +325,6 @@ static void kbase_csf_dump_firmware_trace_buffer(struct kbase_device *kbdev)
 		*p = 0;
 		dev_err(kbdev->dev, "FW> %s", buf);
 	}
-	mutex_unlock(&kbdev->trace_buffer_mutex);
 
 	kfree(buf);
 }
