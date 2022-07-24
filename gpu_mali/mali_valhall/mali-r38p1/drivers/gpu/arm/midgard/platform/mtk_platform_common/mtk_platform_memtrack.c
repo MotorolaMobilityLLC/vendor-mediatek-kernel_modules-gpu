02f08d0b1f7094eb01b27fa1b388c05946d0a399
mtk16965 <holiday.cheng@mediatek.com>
2022-07-24 12:55:42 +0800

    [ALPS07436341] r38p1: common: trylock in gpu_memory show
    [Detail]
    To prevent aee_dump timeout when cat gpu_memory.
    
    We will use mutex_trylock instead of mutex_lock to
    prevent blocking the aee_dump flow.
    
    MTK-Commit-Id: 3bff36713bde4903fb8ca981035b99e16373a910
    
    CR-Id: ALPS07436341
    Feature: ARM GPU DDK driver
    Change-Id: I6a5918a0bca1a66d06906b73d272e96c7a6bda8c

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_memtrack.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_memtrack.c
@@ -11,6 +11,7 @@
 #include <mali_kbase_defs.h>
 #include <platform/mtk_platform_common.h>
 #include <platform/mtk_platform_common/mtk_platform_memtrack.h>
+#include <linux/delay.h>
 
 extern unsigned int (*mtk_get_gpu_memory_usage_fp)(void);
 
@@ -24,6 +25,7 @@ static int mtk_gpu_memory_show(struct seq_file *m, void *v)
 {
 	struct kbase_device *kbdev = (struct kbase_device *)mtk_common_get_kbdev();
 	struct kbase_context *kctx;
+	unsigned int trylock_count = 0;
 
 	if (IS_ERR_OR_NULL(kbdev))
 		return -1;
@@ -32,12 +34,21 @@ static int mtk_gpu_memory_show(struct seq_file *m, void *v)
 
 	mutex_lock(&memtrack_lock);
 
+	while (!mutex_trylock(&kbdev->kctx_list_lock)) {
+		if (trylock_count > 3) {
+			pr_info("[%s]lock held, bypass memory usage query", __func__);
+			seq_printf(m, "<INVALID>");
+			goto out_lock_held;
+		}
+		trylock_count ++;
+		udelay(10);
+	}
+
 	/* output the total memory usage */
 	seq_printf(m, "%-16s  %10u\n",
 	           kbdev->devname,
 	           atomic_read(&(kbdev->memdev.used_pages)));
 
-	mutex_lock(&kbdev->kctx_list_lock);
 	list_for_each_entry(kctx, &kbdev->kctx_list, kctx_list_link) {
 		/* output the memory usage and cap for each kctx */
 		seq_printf(m, "  %s-0x%p %10u %10u\n",
@@ -48,6 +59,7 @@ static int mtk_gpu_memory_show(struct seq_file *m, void *v)
 	}
 	mutex_unlock(&kbdev->kctx_list_lock);
 
+out_lock_held:
 	mutex_unlock(&memtrack_lock);
 
 	lockdep_on();
