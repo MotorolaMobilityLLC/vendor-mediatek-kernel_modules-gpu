241dff567b6cc3801819fa87ef5eaac8698e2f9b
GuanWen Chen <gw.chen@mediatek.com>
2023-01-10 17:03:25 +0800

    [ALPS07803267] DEBUG: Not need to check the power flag when trigger reset
    [Detail]
    Not need to check the power flag when trigger reset
    
    MTK-Commit-Id: 451d811df8b308c89377f894e4eed647214cc450
    
    Signed-off-by: GuanWen Chen <gw.chen@mediatek.com>
    CR-Id: ALPS07803267
    Feature: ARM GPU DDK driver
    Change-Id: I8cfcab40c8657fab6baed691a99c0db18d2c7102

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_debug.c
@@ -2249,25 +2249,24 @@ static void mtk_debug_dump_for_external_fence(int fd, int pid, int type, int tim
 		spin_lock(&kbdev->reset_force_change);
 		kbdev->reset_force_evict_group_work = true;
 		spin_unlock(&kbdev->reset_force_change);
-		if (kbdev->pm.backend.gpu_powered) {
-			if (kbase_prepare_to_reset_gpu(kbdev, RESET_FLAGS_NONE)) {
-				dev_info(kbdev->dev, "External fence timeouts(%d ms)! Trigger GPU reset", timeouts);
+
+		if (kbase_prepare_to_reset_gpu(kbdev, RESET_FLAGS_NONE)) {
+			dev_info(kbdev->dev, "External fence timeouts(%d ms)! Trigger GPU reset", timeouts);
 #if IS_ENABLED(CONFIG_MALI_MTK_LOG_BUFFER)
-				mtk_logbuffer_print(&kbdev->logbuf_exception,
-					"[%llxt] External fence timeouts(%d ms)! Trigger GPU reset\n",
-					mtk_logbuffer_get_timestamp(kbdev, &kbdev->logbuf_exception),
-					timeouts);
+			mtk_logbuffer_print(&kbdev->logbuf_exception,
+				"[%llxt] External fence timeouts(%d ms)! Trigger GPU reset\n",
+				mtk_logbuffer_get_timestamp(kbdev, &kbdev->logbuf_exception),
+				timeouts);
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
-				kbase_reset_gpu(kbdev);
-			} else {
-				dev_info(kbdev->dev, "External fence timeouts(%d ms)! Other threads are already resetting the GPU", timeouts);
+			kbase_reset_gpu(kbdev);
+		} else {
+			dev_info(kbdev->dev, "External fence timeouts(%d ms)! Other threads are already resetting the GPU", timeouts);
 #if IS_ENABLED(CONFIG_MALI_MTK_LOG_BUFFER)
-				mtk_logbuffer_print(&kbdev->logbuf_exception,
-					"[%llxt] External fence timeouts(%d ms)! Other threads are already resetting the GPU\n",
-					mtk_logbuffer_get_timestamp(kbdev, &kbdev->logbuf_exception),
-					timeouts);
+			mtk_logbuffer_print(&kbdev->logbuf_exception,
+				"[%llxt] External fence timeouts(%d ms)! Other threads are already resetting the GPU\n",
+				mtk_logbuffer_get_timestamp(kbdev, &kbdev->logbuf_exception),
+				timeouts);
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
-			}
 		}
 	}
 #endif /* CONFIG_MALI_MTK_TIMEOUT_RESET */
