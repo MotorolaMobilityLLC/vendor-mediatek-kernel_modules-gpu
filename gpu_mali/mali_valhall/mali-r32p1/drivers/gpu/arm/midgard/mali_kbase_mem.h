e066f7d9e4e2fea194a0765f35546f00549b04fa
mtk31598 <ryan.fu@mediatek.com>
2023-02-17 14:36:10 +0800

    [ALPS07589161] GPU: GPUSWERRATA-1420 + 1425 for CVE-2022-36449
    [Description]
    
    [Patch]
    
    [ARM Ticket]
    00487868
    
    MTK-Commit-Id: 9053a2a155d56d74dcb540a4aef903195ac884ec
    
    Signed-off-by: mtk31598 <ryan.fu@mediatek.com>
    CR-Id: ALPS07589161
    Feature: ARM GPU DDK driver
    Change-Id: I36b92a18b491ddc19fd6a5e1d8ae06ecab7edccd
    (cherry-pick from e90f7f5826e891d89259b42f1b591945aa5a0f49)

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_mem.h
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_mem.h
@@ -1256,6 +1256,7 @@ void kbase_mmu_disable_as(struct kbase_device *kbdev, int as_nr);
 
 void kbase_mmu_interrupt(struct kbase_device *kbdev, u32 irq_stat);
 
+#if defined(CONFIG_MALI_VECTOR_DUMP)
 /**
  * kbase_mmu_dump() - Dump the MMU tables to a buffer.
  *
@@ -1275,6 +1276,7 @@ void kbase_mmu_interrupt(struct kbase_device *kbdev, u32 irq_stat);
  * (including if the @c nr_pages is too small)
  */
 void *kbase_mmu_dump(struct kbase_context *kctx, int nr_pages);
+#endif
 
 /**
  * kbase_sync_now - Perform cache maintenance on a memory region
