18b33d860960dc711318d499421f4b20fffa5ccb
Daniel Tao <daniel.tao@mediatek.com>
2023-06-20 02:13:06 +0800

    [ALPS08143548] GPU: GPUSWERRATA-1725
    [Description]
    Commands for GPU cache maintenance and TLB invalidation were sent after
    acquiring hwaccess_lock and checking if the gpu_powered flag is set.
    The combination of lock and the flag ensured that GPU registers remained
    accessible whilst the commands were in progress. If the flag was not set
    then the GPU power up was not performed and the commands were rightfully
    skipped.
    The gpu_powered flag is set immediately after the Top-level power up
    of GPU is done by the platform specific power_on_callback() and so the
    registers can be safely accessed. If the callback returns 1 then a
    silent soft-reset of the GPU is performed after setting the flag.
    
    This lead to a race between the cache maintanence commands and the soft
    reset of GPU, due to which the commands did not complete or got lost
    and there was a timeout.
    
    This commit replaces the gpu_powered flag with the gpu_ready flag
    as the latter is set after the soft-reset is done and all the in-use
    GPU address spaces have been enabled. It is okay to skip the commands
    when the flag is false, as L2 cache would be in powered down state.
    
    The page migrate function is also updated to use gpu_ready flag as
    that was also affected by the similar race with silent reset in
    GPUCORE-35861 and kbdev->pm.lock had to be used.
    
    A defect test has also been added that can reliably trigger the same
    problem on multi core Krake bitfile.
    
    [Patch]
    GPUCORE-37946: Synchronize GPU cache flush cmds with silent reset on GPU power up
    
    [ARM Ticket]
    03427919
    
    MTK-Commit-Id: 1da0c46d474db45faec6234e773f88db79ae618f
    
    CR-Id: ALPS08143548
    Feature: ARM GPU DDK driver
    Change-Id: Ia540712de184840739b41a384ca3e2dba0910f47

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
@@ -161,7 +161,7 @@ static void mmu_invalidate(struct kbase_device *kbdev, struct kbase_context *kct
 
 	spin_lock_irqsave(&kbdev->hwaccess_lock, flags);
 
-	if (kbdev->pm.backend.gpu_powered && (!kctx || kctx->as_nr >= 0)) {
+	if (kbdev->pm.backend.gpu_ready && (!kctx || kctx->as_nr >= 0)) {
 		as_nr = kctx ? kctx->as_nr : as_nr;
 		if (kbase_mmu_hw_do_unlock(kbdev, &kbdev->as[as_nr], op_param))
 			dev_err(kbdev->dev,
@@ -209,7 +209,7 @@ static void mmu_flush_invalidate(struct kbase_device *kbdev, struct kbase_contex
 	mutex_lock(&kbdev->mmu_hw_mutex);
 	spin_lock_irqsave(&kbdev->hwaccess_lock, flags);
 
-	if (kbdev->pm.backend.gpu_powered && (!kctx || kctx->as_nr >= 0)) {
+	if (kbdev->pm.backend.gpu_ready && (!kctx || kctx->as_nr >= 0)) {
 		struct kbase_as *as = &kbdev->as[kctx ? kctx->as_nr : as_nr];
 
 		if (kbase_mmu_hw_do_flush_locked(kbdev, as, op_param))
@@ -242,7 +242,7 @@ static void mmu_flush_invalidate_on_gpu_ctrl(struct kbase_device *kbdev, struct 
 	mutex_lock(&kbdev->mmu_hw_mutex);
 	spin_lock_irqsave(&kbdev->hwaccess_lock, flags);
 
-	if (kbdev->pm.backend.gpu_powered && (!kctx || kctx->as_nr >= 0)) {
+	if (kbdev->pm.backend.gpu_ready && (!kctx || kctx->as_nr >= 0)) {
 		as_nr = kctx ? kctx->as_nr : as_nr;
 		if (kbase_mmu_hw_do_flush_on_gpu_ctrl(kbdev, &kbdev->as[as_nr], op_param))
 			dev_err(kbdev->dev, "Flush for GPU page table update did not complete");
