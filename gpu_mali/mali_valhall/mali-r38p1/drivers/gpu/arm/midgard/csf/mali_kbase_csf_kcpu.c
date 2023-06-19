40c91a78f47153e2faff6617b25f55c4dfa0db00
Joe-CH.Tsai <Joe-CH.Tsai@mediatek.com>
2023-06-20 02:13:05 +0800

    [ALPS08055598] Revise log function for print too much issue
    Revise log function for print too much issue
    
    MTK-Commit-Id: a8f74a16a08faa95888499b58e3b25b2062c5235
    
    CR-Id: ALPS08055598
    Feature: ARM GPU DDK driver
    Change-Id: I52ae0fa54490b19ecffe5b1dc59a4250918b04f2

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
@@ -1843,15 +1843,25 @@ static int kbase_kcpu_fence_signal_process(
 	if (atomic_dec_return(&kcpu_queue->fence_signal_pending_cnt) > 0) {
 		fence_signal_timeout_start(kcpu_queue);
 #ifdef CONFIG_MALI_FENCE_DEBUG
+#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
+		dev_vdbg(kctx->kbdev->dev,
+			 "kbase re-arm KCPU fence signal timeout timer for next signal command");
+#else /* CONFIG_MALI_MTK_DEBUG */
 		dev_info(kctx->kbdev->dev,
 			 "kbase re-arm KCPU fence signal timeout timer for next signal command");
+#endif /* CONFIG_MALI_MTK_DEBUG */
 #endif
 	} else {
 #ifdef CONFIG_MALI_FENCE_DEBUG
 		int del = del_timer_sync(&kcpu_queue->fence_signal_timeout);
 
+#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
+		dev_vdbg(kctx->kbdev->dev, "kbase KCPU delete fence signal timeout timer ret: %d",
+			 del);
+#else /* CONFIG_MALI_MTK_DEBUG */
 		dev_info(kctx->kbdev->dev, "kbase KCPU delete fence signal timeout timer ret: %d",
 			 del);
+#endif /* CONFIG_MALI_MTK_DEBUG */
 #else
 		del_timer_sync(&kcpu_queue->fence_signal_timeout);
 #endif
