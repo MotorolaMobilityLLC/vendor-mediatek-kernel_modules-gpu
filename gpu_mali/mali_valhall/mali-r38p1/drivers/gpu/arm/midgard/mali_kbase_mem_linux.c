0ef2727fb7172e447deedbd50544b9a2ba7d1376
Daniel Tao <daniel.tao@mediatek.com>
2023-06-20 02:13:08 +0800

    [ALPS08143548] GPU: GPUSWERRATA-1643
    [Description]
    On CPUs affected (see notes below), corruption of a read-only user-
    buf is possible at the point it is unmapped, where a more privileged
    process has a writeable mapping into which it has potentially
    performed modifications. During the unmapping of the RO mapping, the
    driver may incorrectly invalidate the RO mapping without also,
    cleaning, leading to the writes from the privileged process being
    lost.
    
    On modern Arm CPUs, this issue cannot be seen because the CPU
    promotes DC IVAC to DC CIVAC, and DCIMVAC to DCCIMVAC. This includes
    Cortex-A15, all Arm v8 CPUs and later architectures.
    
    [Patch]
    GPUCORE-37354 RO userbuf corruption on unmap Cache-inval
    
    [ARM Ticket]
    03427919
    
    MTK-Commit-Id: 4aad009ed50dc43166bc4c374339b37ee1d38719
    
    CR-Id: ALPS08143548
    Feature: ARM GPU DDK driver
    Change-Id: I329624e2af9b751891f4b1d1538244ad061b4397

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_mem_linux.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_mem_linux.c
@@ -1832,7 +1832,8 @@ static struct kbase_va_region *kbase_mem_from_user_buffer(
 		 */
 		for (i = 0; i < faulted_pages; i++) {
 			dma_addr_t dma_addr =
-				dma_map_page_attrs(dev, pages[i], 0, PAGE_SIZE, DMA_BIDIRECTIONAL,
+				dma_map_page_attrs(dev, pages[i], 0, PAGE_SIZE,
+						   write ? DMA_BIDIRECTIONAL : DMA_TO_DEVICE,
 						   DMA_ATTR_SKIP_CPU_SYNC);
 
 			if (dma_mapping_error(dev, dma_addr))
@@ -1861,7 +1862,8 @@ unwind_dma_map:
 		dma_addr_t dma_addr = user_buf->dma_addrs[i];
 
 		dma_sync_single_for_device(dev, dma_addr, PAGE_SIZE, DMA_BIDIRECTIONAL);
-		dma_unmap_page_attrs(dev, dma_addr, PAGE_SIZE, DMA_BIDIRECTIONAL,
+		dma_unmap_page_attrs(dev, dma_addr, PAGE_SIZE,
+				     write ? DMA_BIDIRECTIONAL : DMA_TO_DEVICE,
 				     DMA_ATTR_SKIP_CPU_SYNC);
 	}
 fault_mismatch:
