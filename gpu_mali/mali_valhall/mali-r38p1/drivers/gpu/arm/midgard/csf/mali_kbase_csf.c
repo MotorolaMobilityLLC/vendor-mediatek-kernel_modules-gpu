1a5b4314863458443699ec18bcc3de926718e94b
GuanWen Chen <gw.chen@mediatek.com>
2023-07-05 14:29:05 +0800

    [ALPS08176017] Fix use-after-free issue
    [Detail]
    Fix use-after-free issue
    
    [Solution]
    Remove redundant function
    
    MTK-Commit-Id: dbde09bf82666a6697889a2f6474a0b2bd441f2b
    
    CR-Id: ALPS08176017
    Feature: ARM GPU DDK driver
    Change-Id: I65b3c5f9b7f126949810f8604cd5017d4f9134e0

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf.c
@@ -2763,8 +2763,6 @@ static void process_cs_interrupts(struct kbase_queue_group *const group,
 						"[%llxt] %s %d: OoM event have been already queued\n",
 						mtk_logbuffer_get_timestamp(kbdev, &kbdev->logbuf_exception), __func__, __LINE__);
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
-
-					release_queue(queue);
 				}
 #else
 				if (WARN_ON(!queue_work(wq, &queue->oom_event_work))) {
