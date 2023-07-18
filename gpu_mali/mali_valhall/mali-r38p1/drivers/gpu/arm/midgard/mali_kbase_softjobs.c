3376fd6098054cff84cc602b8e3da2a1a75dadfe
susan.su <susan.su@mediatek.com>
2023-07-18 10:55:22 +0800

    [ALPS08102356] Revert "[ALPS07873521] add fence soft-jobs information"
    [Detail]
    This reverts commit 065a7e6b0f1622f2f02e59efc064a2c25ed595e8.
    Reason for revert: for test
    
    MTK-Commit-Id: b3444986082cbc8da95816f472c5591a00488490
    
    Change-Id: I6d7ffeb6338a5d5a38e6e75a253f460831f75e50
    CR-Id: ALPS08102356
    Feature: ARM GPU DDK driver

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_softjobs.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_softjobs.c
@@ -39,7 +39,6 @@
 #include <linux/sched.h>
 #include <linux/kernel.h>
 #include <linux/cache.h>
-#include <mali_kbase_fence.h>
 
 #if !MALI_USE_CSF
 /**
@@ -330,11 +329,11 @@ static void kbase_fence_debug_wait_timeout(struct kbase_jd_atom *katom)
 		return;
 	}
 
-	dev_warn(dev, "ctx %d_%d: Atom %d still waiting for fence [%p] after %dms\n",
+	dev_warn(dev, "ctx %d_%d: Atom %d still waiting for fence [%pK] after %dms\n",
 		 kctx->tgid, kctx->id,
 		 kbase_jd_atom_id(kctx, katom),
 		 info.fence, timeout_ms);
-	dev_warn(dev, "\tGuilty fence [%p] %s: %s\n",
+	dev_warn(dev, "\tGuilty fence [%pK] %s: %s\n",
 		 info.fence, info.name,
 		 kbase_sync_status_string(info.status));
 
@@ -1615,92 +1614,12 @@ int kbase_process_soft_job(struct kbase_jd_atom *katom)
 	return ret;
 }
 
-static void kbase_sync_fence_debug_dump(struct dma_fence *fence)
-{
-	unsigned long flags;
-	struct kbase_context *kctx = NULL;
-	struct dma_fence_array *fa = to_dma_fence_array(fence);
-
-	spin_lock_irqsave(fence->lock, flags);
-
-	if (fa) {
-		int i;
-		pr_err("mali gpu: Fence is a fence array\n");
-
-		for (i = 0; i < fa->num_fences; ++i) {
-			pr_err("mali gpu: fence_array[%d] - %s - %d\n",
-			       i,
-			       fa->fences[i]->ops->get_driver_name(fa->fences[i]),
-			       dma_fence_is_signaled(fa->fences[i]));
-
-			if (fa->fences[i]->ops == &kbase_fence_ops) {
-				pr_err("mali gpu: Mali fence found inside fence array\n");
-				kctx = *(void**)(fa->fences[i]+1);
-				pr_err("mali gpu: Found kctx %p\n", kctx);
-			}
-		}
-	} else if (fence->ops == &kbase_fence_ops) {
-		kctx = *(void**)(fence+1);
-		pr_err("mali gpu: Fence is a Mali fence\n");
-		pr_err("mali gpu: Found kctx %p\n", kctx);
-	} else {
-		pr_err("mali gpu: Fence is not a Mali fence nor a fence array\n");
-	}
-	spin_unlock_irqrestore(fence->lock, flags);
-}
-
-static void kbase_jd_debug_fence_info(struct kbase_jd_atom *katom)
-{
-#if defined(CONFIG_SYNC) || defined(CONFIG_SYNC_FILE)
-	struct kbase_sync_fence_info info;
-	int res;
-	struct kbase_context *kctx = katom->kctx;
-	struct dma_fence * fence;
-
-	switch (katom->core_req & BASE_JD_REQ_SOFT_JOB_TYPE) {
-	case BASE_JD_REQ_SOFT_FENCE_TRIGGER:
-		res = kbase_sync_fence_out_info_get(katom, &info);
-		if (res == 0) {
-			fence = info.fence;
-			pr_err("mali gpu: ctx %d_%d atom[%d] Sa([%p] %s) context#seqno:%s (driver=%s, timeline=%s)",
-					kctx->tgid, kctx->id, kbase_jd_atom_id(kctx, katom),
-					info.fence, kbase_sync_status_string(info.status), info.name,
-					fence->ops->get_driver_name(fence),
-					fence->ops->get_timeline_name(fence));
-			kbase_sync_fence_debug_dump(info.fence);
-		}
-		break;
-	case BASE_JD_REQ_SOFT_FENCE_WAIT:
-		res = kbase_sync_fence_in_info_get(katom, &info);
-		if (res == 0) {
-			fence = info.fence;
-			pr_err("mali gpu: ctx %d_%d atom[%d] Wa([%p] %s) context#seqno:%s (driver=%s, timeline=%s)",
-					kctx->tgid, kctx->id, kbase_jd_atom_id(kctx, katom),
-					info.fence, kbase_sync_status_string(info.status), info.name,
-					fence->ops->get_driver_name(fence),
-					fence->ops->get_timeline_name(fence));
-			kbase_sync_fence_debug_dump(info.fence);
-		}
-		break;
-	default:
-		break;
-	}
-#endif /* CONFIG_SYNC || CONFIG_SYNC_FILE */
-}
-
 void kbase_cancel_soft_job(struct kbase_jd_atom *katom)
 {
-	kbase_jd_debug_fence_info(katom);
-
 	switch (katom->core_req & BASE_JD_REQ_SOFT_JOB_TYPE) {
 #if defined(CONFIG_SYNC) || defined(CONFIG_SYNC_FILE)
 	case BASE_JD_REQ_SOFT_FENCE_WAIT:
 		kbase_sync_fence_in_cancel_wait(katom);
-		break;
-	case BASE_JD_REQ_SOFT_FENCE_TRIGGER:
-		katom->event_code = BASE_JD_EVENT_JOB_CANCELLED;
-		kbase_sync_fence_out_trigger(katom, katom->event_code ==
-				BASE_JD_EVENT_DONE ? 0 : -EFAULT);
 		break;
 #endif
 	case BASE_JD_REQ_SOFT_EVENT_WAIT:
@@ -1750,9 +1669,6 @@ int kbase_prepare_soft_job(struct kbase_jd_atom *katom)
 				fence.basep.fd = -EINVAL;
 				return -EINVAL;
 			}
-#ifdef CONFIG_MALI_FENCE_DEBUG
-			kbasep_add_waiting_soft_job(katom);
-#endif
 		}
 		break;
 	case BASE_JD_REQ_SOFT_FENCE_WAIT:
