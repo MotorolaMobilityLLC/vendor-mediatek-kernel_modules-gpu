ea2a520aa70adf26792eaaf1d20d41bd931c5c5e
Daniel Tao <daniel.tao@mediatek.com>
2023-08-24 14:42:25 +0800

    [ALPS08181638] GPU: GPUCORE-36682(w/c5651) for CVE-2023-28469
    [Description]
    This commit combines the relevant code changes from:
     1. GPUCORE-30526 Cache-Ops update via GPU Control
     2. GPUCORE-36682 fix
    to R32p1_01eac1, for preventing a potential use after free situation.
    
    Due to code divergence, some targeted adapting is applied.
    
    [Patch]
    GPUCORE-38131 Backport Fix From GPUCORE-36682 to R32p1
    c5651 Restore JM Behaviour In GPUCORE-36682 Fix to R32p1
    
    [ARM Ticket]
    03422243
    
    MTK-Commit-Id: 99efdd14f624444403cca2e754f5b2a064cb8504
    
    CR-Id: ALPS08181638
    Feature: ARM GPU DDK driver
    Change-Id: Iddebff3b723d46d32a4ccd00d437d5630f699d9b
    (cherry-pick from e4a6bf28a46f657b822634b756fcd6d317e9e00b)
    
    Change-Id: I9ee0660bd5d16741b1e876875ded0a63a09f8420

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu_hw.h
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu_hw.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  *
- * (C) COPYRIGHT 2014-2015, 2018-2021 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2014-2015, 2018-2023 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
@@ -83,25 +83,20 @@ int kbase_mmu_hw_do_operation(struct kbase_device *kbdev, struct kbase_as *as,
 		unsigned int handling_irq);
 
 /**
- * kbase_mmu_hw_do_operation_locked - Issue an operation to the MMU.
+ * kbase_mmu_hw_do_lock_op - Issue a lock/unlock operation to the MMU .
  * @kbdev:         kbase device to issue the MMU operation on.
  * @as:            address space to issue the MMU operation on.
  * @vpfn:          MMU Virtual Page Frame Number to start the operation on.
  * @nr:            Number of pages to work on.
- * @type:          Operation type (written to ASn_COMMAND).
- * @handling_irq:  Is this operation being called during the handling
- *                 of an interrupt?
+ * @op_lock:       True for lock operation, otherwise unlock.
  *
- * Issue an operation (MMU invalidate, MMU flush, etc) on the address space that
- * is associated with the provided kbase_context over the specified range
- * Both mmu_hw_mutex and hwaccess_lock need to be held when calling this
- * function.
+ * Issue a lock/unlock operation on the address space that is associated
+ * with the provided kbase_context over the specified range
  *
  * Return: Zero if the operation was successful, non-zero otherwise.
  */
-int kbase_mmu_hw_do_operation_locked(struct kbase_device *kbdev, struct kbase_as *as,
-		u64 vpfn, u32 nr, u32 type,
-		unsigned int handling_irq);
+int kbase_mmu_hw_do_lock_op(struct kbase_device *kbdev, struct kbase_as *as,
+			    u64 vpfn, u32 nr, bool op_lock);
 
 /**
  * kbase_mmu_hw_clear_fault - Clear a fault that has been previously reported by
