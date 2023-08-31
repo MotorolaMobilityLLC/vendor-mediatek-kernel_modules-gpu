532014f1229a1957e8cf1706d428c177e2f5705b
ot905164 <ot_keping.ren@mediatek.com>
2023-08-31 14:52:44 +0800

    [ALPS08326167] Implement D32S8 support for Unity
    Important patch for Vulkan
    1. Resolve Unity vulkan blacklist issue.
    2. V10 (Seasons test).
    3. Call of Duty Mobile.
    
    [Support]
    Ticket:136868
    1. Resolve Unity vulkan blacklist issue:
        PP136878.Change.DDK.version.to.1.15.6133110_KM.patch
        PP136878.Change.DDK.version.to.1.15.6133110_UM.patch
            - Change DDK version to 1.15@6133110 and Unity will check (branch == 1.15 && CL >= 6133110 || branch >= 23.2) to unblock IMG Vulkan.
        PP136878.CL6371550.6434881.6436500.6438043.6441019.6443283.6462571.Implement.D32S8.support.for.Unity_modify.patch
            - Implement D32S8 support which is required for correct rendering in some Unity content.
    2. V10 (Seasons test):
        PP136878.CL6391382.Fixed.unreachable.blocks.for.Nntutu.V10.patch
            - Stop leaving unreachable blocks in the CFG after ISP feedback ("discard") optimizations.
    3. Call of Duty Mobile:
        PP136878.CL6446593.6446636.6450806.Bug.fixes.for.Activision.CODM_v2.patch
            - Support loading from multiple input vertexes in a single OpLoad instruction. Handle out of bounds indexes when setting up variable reference info. Fix off-by-1 in array size check when setting up variable reference info
    
    MTK-Commit-Id: 6dbcd2380f3af5ba19f3042c4533f1eb1ce70330
    
    CR-Id: ALPS08326167
    Feature: IMG GPU DDK driver
    Change-Id: I352f5992b02ea59468f95d0e5401b4137432ae6d
    (cherry picked from commit c5df960a3bd4667e74ab83042226654b0dc8f05a)

--- gpu_rgx/m1.15RTM26133109/include/pvrversion.h
+++ gpu_rgx/m1.15RTM26133109/include/pvrversion.h
@@ -49,17 +49,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #define PVRVERSION_FAMILY           "rogueddk"
 #define PVRVERSION_BRANCHNAME       "1.15"
-#define PVRVERSION_BUILD             6133109
+#define PVRVERSION_BUILD             6133110
 #define PVRVERSION_BSCONTROL        "Rogue_DDK_Android"
 
-#define PVRVERSION_STRING           "Rogue_DDK_Android rogueddk 1.15@6133109"
-#define PVRVERSION_STRING_SHORT     "1.15@6133109"
+#define PVRVERSION_STRING           "Rogue_DDK_Android rogueddk 1.15@6133110"
+#define PVRVERSION_STRING_SHORT     "1.15@6133110"
 
 #define COPYRIGHT_TXT               "Copyright (c) Imagination Technologies Ltd. All Rights Reserved."
 
 #define PVRVERSION_BUILD_HI          613
-#define PVRVERSION_BUILD_LO          3109
-#define PVRVERSION_STRING_NUMERIC   "1.15.613.3109"
+#define PVRVERSION_BUILD_LO          3110
+#define PVRVERSION_STRING_NUMERIC   "1.15.613.3110"
 
 #define PVRVERSION_PACK(MAJOR,MINOR) (((IMG_UINT32)((IMG_UINT32)(MAJOR) & 0xFFFFU) << 16U) | (((MINOR) & 0xFFFFU) << 0U))
 #define PVRVERSION_UNPACK_MAJ(VERSION) (((VERSION) >> 16U) & 0xFFFFU)
