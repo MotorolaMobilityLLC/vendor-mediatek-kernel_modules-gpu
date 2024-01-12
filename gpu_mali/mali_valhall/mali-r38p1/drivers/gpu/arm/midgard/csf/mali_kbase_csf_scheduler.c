9e31453af30abbed8fd22f24263e075151b83ed3
mtk28254 <shenghao.lin@mediatek.com>
2024-01-12 20:27:32 +0800

    [ALPS07935048] ACP: DSU power handle for system suspend
    Add DSU power API in system suspend callback since L2 will be powered up for suspending CSGs.
    
    MTK-Commit-Id: 36ed6ff7d014c87cf2e41bc76b14f05ba7d983ac
    
    CR-Id: ALPS07935048
    Feature: ARM GPU DDK middleware
    Change-Id: Iefa5340eb87d80c32eb1835e5f2b3cd67d6cfe38

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
@@ -33,7 +33,7 @@
 #include <mali_kbase_hwaccess_time.h>
 #include "mali_kbase_csf_tiler_heap.h"
 
-#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
+#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG) || IS_ENABLED(CONFIG_MALI_MTK_ACP_DSU_REQ)
 #include <platform/mtk_platform_common.h>
 #endif /* CONFIG_MALI_MTK_DEBUG */
 
@@ -6876,11 +6876,17 @@ int kbase_csf_scheduler_pm_suspend_no_lock(struct kbase_device *kbdev)
 	 */
 	if (scheduler->state == SCHED_SLEEPING) {
 		dev_info(kbdev->dev, "Activating MCU out of sleep on system suspend");
+#if IS_ENABLED(CONFIG_MALI_MTK_ACP_DSU_REQ)
+		mtk_platform_cpu_cache_request(kbdev, REQ_DSU_POWER_ON);
+#endif
 		result = force_scheduler_to_exit_sleep(kbdev);
 		if (result) {
 			dev_warn(kbdev->dev, "Scheduler failed to exit from sleep");
 			goto exit;
 		}
+#if IS_ENABLED(CONFIG_MALI_MTK_ACP_DSU_REQ)
+		mtk_platform_cpu_cache_request(kbdev, REQ_DSU_POWER_OFF);
+#endif
 	}
 #endif
 	if (scheduler->state != SCHED_SUSPENDED) {
