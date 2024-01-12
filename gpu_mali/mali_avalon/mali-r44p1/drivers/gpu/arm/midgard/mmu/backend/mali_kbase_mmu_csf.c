10603535b19ba816df7c466cd343baca9d7f20cc
mtk24487 <carol-cj.wu@mediatek.com>
2024-01-12 20:49:35 +0800

    [ALPS08261002] r44p1[c5717]: Fix kbase fault handling deadlock
    [Detail]
    ARM Ticket: 03437844
    ARM Patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/5717
    If happening gpu bus fault and page fault, the kbase fault handling workqueue will deadlock between page fault work and bus fault work, and the gpu reset worker will be blocked forever.
    
    MTK-Commit-Id: 0abd830c9dd0d9cd9f24d86881655ec377e025c0
    
    CR-Id: ALPS08261002
    Feature: ARM GPU DDK driver
    Change-Id: I16e745555e8cb4ada374455b563fdcb34039b9dc

--- gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/backend/mali_kbase_mmu_csf.c
+++ gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/backend/mali_kbase_mmu_csf.c
@@ -1055,7 +1055,7 @@ int kbase_mmu_as_init(struct kbase_device *kbdev, unsigned int i)
 	kbdev->as[i].gf_data.addr = 0ULL;
 	kbdev->as[i].is_unresponsive = false;
 
-	kbdev->as[i].pf_wq = alloc_workqueue("mali_mmu%d", WQ_UNBOUND, 1, i);
+	kbdev->as[i].pf_wq = alloc_workqueue("mali_mmu%d", WQ_UNBOUND, 0, i);
 	if (!kbdev->as[i].pf_wq)
 		return -ENOMEM;
 
