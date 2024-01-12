23d7acb9aa977e5f9bbe15061aa10a5448143ad8
mtk31598 <ryan.fu@mediatek.com>
2024-01-12 20:22:59 +0800

    [ALPS07885899] GPU: GPUSWERRATA-1551 for CVE-2022-46396
    [Description]
    If user space injects incorrect value ( >= MEMORY_GROU_MANAGER_NR_GROUPS ) for group_id
    via KBASE_IOCTL_CS_TILER_HEAP_INIT or KBASE_IOCTL_CS_TILER_HEAP_INIT_1_13 IOCTLs,
    kernel driver simply accepts it without validating when initializing CSF tiler heap memory.
    Later on it could lead to kernel panic due to out-of-bound access against
    the array of memory pools maintained in kernel space.
    
    [Patch]
    GPUCORE-36665
    
    [ARM Ticket]
    03400817
    
    MTK-Commit-Id: 8bf1505bf668fa5f23868d2d79df2d738a24609e
    
    Signed-off-by: mtk31598 <ryan.fu@mediatek.com>
    CR-Id: ALPS07885899
    Feature: ARM GPU DDK driver
    Change-Id: Ic912c34556eafcef4e0989fe1a794927072516aa
    (cherry picked from commit 99f627fac6aab97fe3d47be8d16e5591d0124e81)

--- gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ gpu_mali/mali_valhall/mali-r38p1/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -1577,6 +1577,9 @@ static int kbasep_kcpu_queue_enqueue(struct kbase_context *kctx,
 static int kbasep_cs_tiler_heap_init(struct kbase_context *kctx,
 		union kbase_ioctl_cs_tiler_heap_init *heap_init)
 {
+	if (heap_init->in.group_id >= MEMORY_GROUP_MANAGER_NR_GROUPS)
+		return -EINVAL;
+
 	kctx->jit_group_id = heap_init->in.group_id;
 
 	return kbase_csf_tiler_heap_init(kctx, heap_init->in.chunk_size,
@@ -1589,6 +1592,9 @@ static int kbasep_cs_tiler_heap_init(struct kbase_context *kctx,
 static int kbasep_cs_tiler_heap_init_1_13(struct kbase_context *kctx,
 					  union kbase_ioctl_cs_tiler_heap_init_1_13 *heap_init)
 {
+	if (heap_init->in.group_id >= MEMORY_GROUP_MANAGER_NR_GROUPS)
+		return -EINVAL;
+
 	kctx->jit_group_id = heap_init->in.group_id;
 
 	return kbase_csf_tiler_heap_init(kctx, heap_init->in.chunk_size,
