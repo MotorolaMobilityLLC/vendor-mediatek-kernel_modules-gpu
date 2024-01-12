fb99075101deefb77f08d0d3996802405e073d0a
Yanqiong Tao <yanqiong.tao@mediatek.com>
2024-01-13 03:47:58 +0800

    [ALPS08657724] Fix security issue CVE-2023-4969
    [Detail]
    CVE-2023-4969 : GPU - Leftover locals - local memory data leak
    
    The user-mode drivers and firmware of the GPU have been updated to introduce protection to prevent this misuse.
    This is achieved by running a special kernel at the end of workloads that use local memory. This special kernel zeroes all local
    memory so that later workloads cannot read previous sensitive data.
    This zeroes all local memory on volcanic cores. On some rogue cores there can be a limited % of local memory that is not zeroed.
    This could allow a subset of previous local memory contents to be read, depending on device dynamics.
    
    Ticket:148649
    
    MTK-Commit-Id: 166b1c65fc6e70e3585dfb6371eb4bcc6de8ccb6
    
    CR-Id: ALPS08657724
    Feature: IMG GPU DDK driver
    Change-Id: If33c964a6466d77a33519145dfa20ed4211dd2a4

--- gpu_rgx/m1.15RTM26133109/services/server/common/physmem.c
+++ gpu_rgx/m1.15RTM26133109/services/server/common/physmem.c
@@ -297,9 +297,10 @@ PVRSRV_ERROR PhysMemValidateParams(IMG_UINT32 ui32NumPhysChunks,
 		#undef X
 			break;
 		default:
+			/* print as 64-bit value to avoid Smatch warning */
 			PVR_LOG_VA(PVR_DBG_ERROR,
-			           "page size of %u is invalid.",
-			           1 << uiLog2AllocPageSize);
+			           "page size of %" IMG_UINT64_FMTSPEC " is invalid.",
+			           IMG_UINT64_C(1) << uiLog2AllocPageSize);
 			return PVRSRV_ERROR_INVALID_PARAMS;
 	}
 
@@ -309,7 +310,7 @@ PVRSRV_ERROR PhysMemValidateParams(IMG_UINT32 ui32NumPhysChunks,
 		PVR_LOG_VA(PVR_DBG_ERROR,
 				 "PMR size exceeds limit #Chunks: %u ChunkSz %"IMG_UINT64_FMTSPECX"",
 				 ui32NumVirtChunks,
-				 (IMG_UINT64) 1ULL << uiLog2AllocPageSize);
+				 (IMG_UINT64) IMG_UINT64_C(1) << uiLog2AllocPageSize);
 		return PVRSRV_ERROR_PMR_TOO_LARGE;
 	}
 
