57119a6ed2b8d065b7039435b8c3ed1172f143a4
mtk16965 <holiday.cheng@mediatek.com>
2024-01-12 20:32:02 +0800

    [ALPS07888312] r44p0: Add lock in ARM kcpu queue dump
    [Detail]
    Add lock in ARM kcpu queue dump to prevent the command
    object is destroyed when using.
    
    MTK-Commit-Id: ab742e431045e41ab0bfbccfefb596ba41ae114a
    
    CR-Id: ALPS07888312
    Feature: ARM GPU DDK driver
    Change-Id: Ia82450a815513f264942f00f1850408724e9d55d

--- gpu_mali/mali_avalon/mali-r44p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
+++ gpu_mali/mali_avalon/mali-r44p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
@@ -1940,7 +1940,11 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 	struct kbase_kcpu_command_queue *queue =
 		container_of(data, struct kbase_kcpu_command_queue, dump_work);
 	struct kbase_context *kctx = queue->kctx;
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+	struct kbase_kcpu_command *cmd;
+#else
 	struct kbase_kcpu_command *cmd = &queue->commands[queue->start_offset];
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 	struct kbase_kcpu_command_fence_info *fence_info;
 	struct kbase_kcpu_dma_fence *kcpu_fence;
 #if (KERNEL_VERSION(4, 10, 0) > LINUX_VERSION_CODE)
@@ -1952,6 +1956,10 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 	size_t i;
 
 	mutex_lock(&kctx->csf.kcpu_queues.lock);
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+	mutex_lock(&queue->lock);
+	cmd = &queue->commands[queue->start_offset];
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 
 	/* Find the next fence signal command in the queue */
 	for (i = 0; i < queue->num_pending_cmds - 1; i++) {
@@ -1964,6 +1972,9 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 		dev_err(kctx->kbdev->dev,
 			"%s: No fence signal command found in ctx:%d_%d kcpu queue:%u", __func__,
 			kctx->tgid, kctx->id, queue->id);
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+		mutex_unlock(&queue->lock);
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 		mutex_unlock(&kctx->csf.kcpu_queues.lock);
 		return;
 	}
@@ -1974,6 +1985,9 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 	if (!fence) {
 		dev_err(kctx->kbdev->dev, "no fence found in ctx:%d_%d kcpu queue:%u", kctx->tgid,
 			kctx->id, queue->id);
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+		mutex_unlock(&queue->lock);
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 		mutex_unlock(&kctx->csf.kcpu_queues.lock);
 		return;
 	}
@@ -1983,6 +1997,9 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 		dev_err(kctx->kbdev->dev, "no fence metadata found in ctx:%d_%d kcpu queue:%u",
 			kctx->tgid, kctx->id, queue->id);
 		kbase_fence_put(fence);
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+		mutex_unlock(&queue->lock);
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 		mutex_unlock(&kctx->csf.kcpu_queues.lock);
 		return;
 	}
@@ -2015,6 +2032,7 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 			fence->ops->get_driver_name(fence), fence->ops->get_timeline_name(fence));
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
 
+		mutex_unlock(&queue->lock);
 		kbasep_csf_sync_kcpu_dump_locked(kctx, NULL);
 		mutex_unlock(&kctx->csf.kcpu_queues.lock);
 
@@ -2023,6 +2041,7 @@ static void kcpu_queue_dump_worker(struct work_struct *data)
 		mtk_common_debug(MTK_COMMON_DBG_CSF_DUMP_GROUPS_QUEUES, kctx->tgid, MTK_DBG_HOOK_MALI_FENCE_SIGNAL_TIMEOUT);
 #endif /* CONFIG_MALI_MTK_DEBUG */
 	} else {
+		mutex_unlock(&queue->lock);
 		mutex_unlock(&kctx->csf.kcpu_queues.lock);
 	}
 #else /* CONFIG_MALI_MTK_FENCE_DEBUG */
