cc83d450e74967ded8c95eda84946088c0c98427
Eric Chung <eric.chung@mediatek.com>
2024-01-12 20:28:20 +0800

    [ALPS07820538] gpu: common: update gpufreq interface
    * Remove power control argument.
    * Update last OPP index to GED sysram.
    
    MTK-Commit-Id: ea3febe20c1964c9a6c6b49969a9ba1e4110e2a0
    
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS07820538
    Feature: MFGSYS driver
    Change-Id: I00af8793ce69523c570e6666ac30596595b75b23

--- gpu_mali/mali_avalon/mali-r44p0/drivers/gpu/arm/midgard/platform/mtk_platform_common.h
+++ gpu_mali/mali_avalon/mali-r44p0/drivers/gpu/arm/midgard/platform/mtk_platform_common.h
@@ -58,9 +58,15 @@ void mtk_common_dump_db(int pid, u64 mask);
 int mtk_common_gpufreq_bringup(void);
 int mtk_common_gpufreq_commit(int opp_idx);
 int mtk_common_ged_dvfs_get_last_commit_idx(void);
+int mtk_common_ged_dvfs_get_last_commit_top_idx(void);
+int mtk_common_ged_dvfs_get_last_commit_stack_idx(void);
 
 unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_idx(void);
+unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_top_idx(void);
+unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_stack_idx(void);
 unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_idx_test(int commit_idx);
+unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_top_idx_test(int commit_idx);
+unsigned long mtk_common_ged_dvfs_write_sysram_last_commit_stack_idx_test(int commit_idx);
 
 int mtk_common_device_init(struct kbase_device *kbdev);
 void mtk_common_device_term(struct kbase_device *kbdev);
