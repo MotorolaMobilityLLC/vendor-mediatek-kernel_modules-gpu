c3ced4a938b03c72691af25f51ee95dac26f6031
yan-hong.zhang <yan-hong.zhang@mediatek.com>
2024-01-29 21:45:18 +0800

    [ALPS08621014] r48p0: Add whitebox test case for sync update worker
    * Enable node to dump the error msg when fw is not ready on sync update
    * Add mode 1 to print error msg
    * Add mode 2 to enqueue sync update worker after gpu idle
    * Add mode 3 to enqueue sync update worker manually
    
    MTK-Commit-Id: 9764d1bc99b4e0e2a1041405338af615172292d8
    
    CR-Id: ALPS08621014
    Feature: ARM GPU DDK driver
    Change-Id: I1c746e3296934a0c09b2947c46bae3ca25ca6869

--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/platform/mtk_platform_common.h
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/platform/mtk_platform_common.h
@@ -102,4 +102,8 @@ void mtk_platform_cpu_cache_request(struct kbase_device *kbdev, int request);
 bool mtk_common_whitebox_force_hard_reset_enable(void);
 #endif /* CONFIG_MALI_MTK_WHITEBOX_FORCE_HARD_RESET */
 
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE)
+int mtk_common_whitebox_sync_update_test_mode(void);
+#endif /* CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE */
+
 #endif /* __MTK_PLATFORM_COMMON_H__ */
--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
@@ -78,6 +78,10 @@ static struct proc_dir_entry *proc_root;
 #include <platform/mtk_platform_common/mtk_platform_whitebox_fault_worker.h>
 #endif /* CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER */
 
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE)
+#include <platform/mtk_platform_common/mtk_platform_whitebox_sync_update.h>
+#endif /* CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE */
+
 static bool mfg_powered;
 static DEFINE_MUTEX(mfg_pm_lock);
 static DEFINE_MUTEX(common_debug_lock);
@@ -114,6 +118,13 @@ bool mtk_common_whitebox_force_hard_reset_enable(void)
 	return mtk_whitebox_force_hard_reset_enable();
 }
 #endif /* CONFIG_MALI_MTK_WHITEBOX_FORCE_HARD_RESET */
+
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE)
+int mtk_common_whitebox_sync_update_test_mode(void)
+{
+	return mtk_whitebox_sync_update_test_mode();
+}
+#endif /* CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE */
 
 #if IS_ENABLED(CONFIG_MALI_MTK_DIAGNOSIS_MODE)
 #if IS_ENABLED(CONFIG_MALI_MTK_POWER_TRANSITION_TIMEOUT_DEBUG)
@@ -552,6 +563,9 @@ void mtk_common_debugfs_init(struct kbase_device *kbdev)
 #if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER)
 	mtk_whitebox_fault_worker_debugfs_init(kbdev);
 #endif /* CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER */
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE)
+	mtk_whitebox_sync_update_test_debugfs_init(kbdev);
+#endif /* CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE */
 }
 
 #if IS_ENABLED(CONFIG_MALI_CSF_SUPPORT)
@@ -631,6 +645,10 @@ int mtk_common_device_init(struct kbase_device *kbdev)
 #if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_FORCE_HARD_RESET)
 	mtk_whitebox_force_hard_reset_init();
 #endif /* CONFIG_MALI_MTK_WHITEBOX_FORCE_HARD_RESET */
+
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE)
+	mtk_whitebox_sync_update_test_init();
+#endif /* CONFIG_MALI_MTK_WHITEBOX_SYNC_UPDATE */
 
 	return 0;
 }
