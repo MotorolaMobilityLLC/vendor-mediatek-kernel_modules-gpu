723853b496b738cbe46cc114303a869e6af6678c
Susan Su <Susan.Su@mediatek.com>
2024-02-19 14:58:31 +0800

    [ALPS08697587] r48p0: use get_file_rcu by kernel version
    [Detail]
    for meet get_file_rcu of kernel-6.7 change,
    use get_file_rcu by kernel version.
    
    MTK-Commit-Id: ffaae9407176225b7ff6a41aa0c493046eda00da
    
    CR-Id: ALPS08697587
    Feature: ARM GPU DDK driver
    Change-Id: Ice04f601ab132b66d4a2878301cb8f1eda7ff4f8

--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/mali_kbase_debug_mem_view.c
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/mali_kbase_debug_mem_view.c
@@ -237,7 +237,11 @@ static int debug_mem_open(struct inode *i, struct file *file)
 	int ret;
 	enum kbase_memory_zone idx;
 
+#if (KERNEL_VERSION(6, 7, 0) <= LINUX_VERSION_CODE)
+	if (!get_file_rcu(&kctx->filp))
+#else
 	if (get_file_rcu(kctx->filp) == 0)
+#endif
 		return -ENOENT;
 
 	/* Check if file was opened in write mode. GPU memory contents
