f8859ffebd1e8c12ebb4ad33a6e8a7820aa87dd0
mtk15701 <chien-lun.hsu@mediatek.com>
2024-03-06 10:10:58 +0800

    [ALPS08742618] [c6476/2] Fix allocate failed error handling
    [Description]
    ARM ticket: 03462268
    ARM patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/6476/2
    
    The sub page already calcuated when the first creation,
    so here we always need to remove the sub_page allocate or it
    will be calculated twice.
    
    [Test]
    GPU_Multimedia_Basic_Sanity PASS
    
    MTK-Commit-Id: 089eff7a82b598b420b6ea3d8d6f4db5d0fc37eb
    
    CR-Id: ALPS08742618
    Feature: ARM GPU DDK driver
    Change-Id: Ia5c4f2daa62e949b491312a42effbf64b4a0dffe

--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/mali_kbase_mem.c
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/mali_kbase_mem.c
@@ -1269,6 +1269,7 @@ int kbase_alloc_phy_pages_helper(struct kbase_mem_phy_alloc *alloc, size_t nr_pa
 	 * to satisfy the memory allocation request.
 	 */
 	size_t nr_pages_to_account = 0;
+	size_t nr_pages_from_sub_page = 0;
 
 	if (WARN_ON(alloc->type != KBASE_MEM_TYPE_NATIVE) ||
 	    WARN_ON(alloc->imported.native.kctx == NULL) ||
@@ -1361,6 +1362,7 @@ int kbase_alloc_phy_pages_helper(struct kbase_mem_phy_alloc *alloc, size_t nr_pa
 							      FROM_PARTIAL);
 #endif /* CONFIG_MALI_MTK_PAGE_TABLE_CLUSTERING */
 					nr_left--;
+					nr_pages_from_sub_page++;
 
 					if (bitmap_full(sa->sub_pages,
 							NUM_PAGES_IN_2MB_LARGE_PAGE)) {
@@ -1501,22 +1503,31 @@ alloc_failed:
 #if IS_ENABLED(CONFIG_MALI_MTK_PAGE_TABLE_CLUSTERING)
 		*apc = curr_apc;
 #endif /* CONFIG_MALI_MTK_PAGE_TABLE_CLUSTERING */
-	}
 
-	/* Undo the preliminary memory accounting that was done early on
-	 * in the function. If only small pages are used: nr_left is equal
-	 * to nr_pages_requested. If a combination of 2 MB and small pages was
-	 * attempted: nr_pages_requested is equal to the sum of nr_left
-	 * and nr_pages_to_free, and the latter has already been freed above.
-	 *
-	 * Also notice that there's no need to update the page count
-	 * because memory allocation was rolled back.
-	 */
-	mem_account_dec(kctx, nr_left);
+		/* Undo the preliminary memory accounting that was done early on
+		 * in the function. If only small pages are used: nr_left is equal
+		 * to nr_pages_requested. If a combination of 2 MB and small pages was
+		 * attempted: nr_pages_requested is equal to the sum of nr_left
+		 * and nr_pages_to_free, and the latter has already been freed above.
+		 * but the sub page already calcuated when the first creation,
+		 * so here we always need to remove the sub_page allocate or it
+		 * will be calculated twice.
+		 *
+		 * Also notice that there's no need to update the page count
+		 * because memory allocation was rolled back.
+		 */
+		mem_account_dec(kctx, nr_left + nr_pages_from_sub_page);
 
 #if IS_ENABLED(CONFIG_MALI_MTK_MEMORY_DEBUG)
-	kbase_trace_free_pages(kbdev->id, kctx, nr_left, (size_t)alloc->pages, alloc->category);
+		kbase_trace_free_pages(kbdev->id, kctx, (nr_left + nr_pages_from_sub_page), (size_t)alloc->pages, alloc->category);
 #endif /* CONFIG_MALI_MTK_MEMORY_DEBUG */
+	} else {
+		mem_account_dec(kctx, nr_left);
+
+#if IS_ENABLED(CONFIG_MALI_MTK_MEMORY_DEBUG)
+		kbase_trace_free_pages(kbdev->id, kctx, nr_left, (size_t)alloc->pages, alloc->category);
+#endif /* CONFIG_MALI_MTK_MEMORY_DEBUG */
+	}
 
 invalid_request:
 	return -ENOMEM;
