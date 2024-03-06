4f9a1fee6f3259b5171346ed2fb951c35d489ef8
mtk24487 <carol-cj.wu@mediatek.com>
2024-03-06 10:10:00 +0800

    [ALPS08334544] r48p0: Refine GPU Protected Meomroy
    1. Save protected memory for fw init
    2. Add pma log
    
    MTK-Commit-Id: 5fcee50c5d3c384da7a9d284916c2d9a53ca4b30
    
    CR-Id: ALPS08334544
    Feature: ARM GPU DDK driver
    Signed-off-by: mtk24487 <carol-cj.wu@mediatek.com>
    Change-Id: Ia447e3074cffd6b88a27dce52914403c20cf37d4

--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_firmware.c
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_firmware.c
@@ -709,6 +709,14 @@ static int parse_memory_setup_entry(struct kbase_device *kbdev,
 		return 0;
 	}
 
+#if IS_ENABLED(CONFIG_MALI_MTK_PROTECTED_PATCH)
+	if (protected_mode) {
+		/* Use small prot/normal pages */
+		force_small_page = true;
+		dev_err(kbdev->dev, "Set force_small_page of prot mem when fw init (%x)", kbdev->pagesize_2mb);
+	}
+#endif /* CONFIG_MALI_MTK_PROTECTED_PATCH */
+
 	num_pages = (virtual_end - virtual_start) >> PAGE_SHIFT;
 
 retry_alloc:
