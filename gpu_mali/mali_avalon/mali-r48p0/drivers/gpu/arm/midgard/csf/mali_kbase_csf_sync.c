0a9e68aad649c0077a0c5822f6b7a7f7c3455b8a
mtk16965 <holiday.cheng@mediatek.com>
2024-04-01 20:29:40 +0800

    [ALPS08821685] r48p0: Adjust ARM kcpu queue dump
    [Description]
    Adjust ARM kcpu queue dump to prevent complicated shown infomation
    
    [Test]
    Whitebox
    
    MTK-Commit-Id: 26cd96d1e01afaaccc71883fd70b8735e0f942db
    
    CR-Id: ALPS08821685
    Feature: ARM GPU DDK driver
    Change-Id: Ic3a41a7087a2a70b4bfa63582cf91f2902b8825c

--- gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_sync.c
+++ gpu_mali/mali_avalon/mali-r48p0/drivers/gpu/arm/midgard/csf/mali_kbase_csf_sync.c
@@ -121,9 +121,15 @@ static void kbasep_csf_sync_print_kcpu_fence_wait_or_signal(char *buffer, int *l
 			    "cmd:%s obj:0x%pK live_value:0x%.8x | ", cmd_name, fence, is_signaled);
 
 	/* Note: fence->seqno was u32 until 5.1 kernel, then u64 */
+#if IS_ENABLED(CONFIG_MALI_MTK_FENCE_DEBUG)
+	*length += snprintf(buffer + *length, CSF_SYNC_DUMP_SIZE - *length,
+			    "timeline_name:%s context#seqno:%s",
+			    timeline_name, info.name);
+#else /* CONFIG_MALI_MTK_FENCE_DEBUG */
 	*length += snprintf(buffer + *length, CSF_SYNC_DUMP_SIZE - *length,
 			    "timeline_name:%s timeline_context:0x%.16llx fence_seqno:0x%.16llx",
 			    timeline_name, fence->context, (u64)fence->seqno);
+#endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 
 	kbase_fence_put(fence);
 }
