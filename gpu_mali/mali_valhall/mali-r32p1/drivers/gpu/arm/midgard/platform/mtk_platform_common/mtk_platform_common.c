510a5dcb06edb828052e0b47f18fe1ff7823aec6
Huan Feng <huan.feng@mediatek.com>
2024-04-15 13:23:31 +0800

    [ALPS08707212] GPU: change for shader on/off (v3)
    [Description]
    shader power-on/off clock change for PLL inside
    => Change clock-source to 26mhz before shader power-on/off
    [Test]
    Build pass
    
    MTK-Commit-Id: f84c6f3a25c7df08791019bd7f3f2cb44137d754
    
    CR-Id: ALPS08707212
    Feature: ARM GPU DDK driver
    Change-Id: I2167c2c4adc3a199c2a0e9266e685bcc54bd677a

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
@@ -319,3 +319,33 @@ void mtk_common_device_term(struct kbase_device *kbdev)
 	MTKGPUPower_model_destroy();
 #endif
 }
+
+#ifdef SHADER_PWR_CTL_WA
+void mtk_set_gpufreq_clock_parking_lock(unsigned long *pFlags)
+{
+	_gpufreq_clock_parking_lock(pFlags);
+}
+
+void mtk_set_gpufreq_clock_parking_unlock(unsigned long *pFlags)
+{
+	_gpufreq_clock_parking_unlock(pFlags);
+}
+
+int mtk_set_gpufreq_clock_parking(int clksrc)
+{
+	/*
+	 * This function will be called under the Interrupt-Handler,
+	 * so can't implement any mutex-lock behaviors
+	 * (that will result the sleep/schedule operations).
+	 */
+
+	int ret = 0;
+
+	if (mtk_common_pm_is_mfg_active())
+		ret = _gpufreq_clock_parking(clksrc);
+	else
+		pr_info("MALI: set clock parking at power off\n");
+
+	return ret;
+}
+#endif
