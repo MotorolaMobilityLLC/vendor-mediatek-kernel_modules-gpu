745945bbd9bb0b9cbc3dfd1ec2bbea88f0a74303
kuldeep.chaudhary <kuldeep.chaudhary@mediatek.com>
2024-05-13 14:46:16 +0800

    [ALPS08694711] MT6833: Boot exception GPU kernel modules
    [Description]
    Boot exception and error
    RenderEngine: mali 13000000.mali: Stride 72 passed to job_submit isnt supported by the kernel
    
    [Test]
    Build pass and probe done
    
    MTK-Commit-Id: df572dd4560b4b0b9597c8aa9270e0ec19b63df0
    
    CR-Id: ALPS08694711
    Feature: ARM GPU DDK driver
    Change-Id: I4e4489c29d43baccb0c7562e175b3ae5ac0ebfcf

--- gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
+++ gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
@@ -831,7 +831,7 @@ struct base_jd_atom_v2 {
 	base_jd_core_req core_req;
 	__u8 renderpass_id;
 	__u8 padding[7];
-#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+#if (defined(CONFIG_MALI_MTK_GPU_BM_JM) || IS_ENABLED(CONFIG_MTK_GPU_MT6833_SUPPORT))
 	u32 frame_nr;  /* frame number to the atom */
 #endif
 };
@@ -882,7 +882,7 @@ typedef struct base_jd_atom {
 	base_jd_core_req core_req;
 	__u8 renderpass_id;
 	__u8 padding[7];
-#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+#if (defined(CONFIG_MALI_MTK_GPU_BM_JM) || IS_ENABLED(CONFIG_MTK_GPU_MT6833_SUPPORT))
 	u32 frame_nr;  /* frame number to the atom */
 #endif
 } base_jd_atom;
