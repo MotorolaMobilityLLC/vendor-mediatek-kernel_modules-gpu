b79b6d8c795779a03650c3a2b13b4c4c1bbe4f23
sourav.sardana <sourav.sardana@mediatek.com>
2024-05-28 10:20:42 +0800

    [ALPS08963405] GPU:Fix dvfs policy not work
    [Description]
    Kernel-6.6 need use GED_POWER_ON(2) to update status to ged when power on
    [Test]
    Boot up
    
    MTK-Commit-Id: 72013d47346d6d112d12d82bae3ddbea78c839eb
    
    Change-Id: Iffc426da20c9f38f6f19b9ddbbf366d9cdaab652
    CR-Id: ALPS08963405
    Feature: ARM GPU DDK driver

--- gpu_rgx/m24.1ED6525005/services/system/rogue/rgx_mtk/mt6765/mtk_mfgsys.c
+++ gpu_rgx/m24.1ED6525005/services/system/rogue/rgx_mtk/mt6765/mtk_mfgsys.c
@@ -226,7 +226,7 @@ static void MTKEnableMfgClock(PVRSRV_POWER_FLAGS ePwrFlags)
 
 	if (gpu_debug_enable)
 		PVR_DPF((PVR_DBG_ERROR, "MTKEnableMfgClock"));
-	ged_dvfs_gpu_clock_switch_notify(1);
+	ged_dvfs_gpu_clock_switch_notify(GED_POWER_ON);
 	mtk_notify_gpu_power_change(1);
 }
 #define MFG_BUS_IDLE_BIT (1 << 16)
@@ -235,7 +235,7 @@ static void MTKDisableMfgClock(PVRSRV_POWER_FLAGS ePwrFlags)
 {
 	int buck_state;
 	mtk_notify_gpu_power_change(0);
-	ged_dvfs_gpu_clock_switch_notify(0);
+	ged_dvfs_gpu_clock_switch_notify(GED_POWER_OFF);
 
 #ifdef MTCMOS_CONTROL
 	gpufreq_power_control(GPU_PWR_OFF);
