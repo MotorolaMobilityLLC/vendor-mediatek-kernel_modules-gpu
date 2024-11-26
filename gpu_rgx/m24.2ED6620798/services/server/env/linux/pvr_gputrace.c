ca0a331f5dd4caf1680f5f33c913be927cad3f98
akansha.shukla <akansha.shukla@mediatek.com>
2024-11-26 11:43:50 +0800

    [ALPS09561468]  Fix CtsStatsdHostTestCases
    [Description]
    
    This CL reverts IMGs WA patch on 9903063 and merges official patch fix: PP171258.CL6662946.Services.Android.disable.tracing_on-KM.patch
    Services: Android: disable tracing_on
    
    The Android profiling tool independently handles ftrace, while systrace used to rely on the atrace HAL for management.
    With systrace no longer in use, perfetto directly controls ftrace instead. If ftrace is enabled by default, perfetto will fail to capture any data. As for the Android framework, it uses eBPF to collect tracepoint data. And also, the API has been dropped in Android common kernels [1].
    [1]: https://android-review.googlesource.com/c/kernel/common/+/3197258
    
    [Test]
    CtsStatsdHostTestCases  pass
    
    [Ticket]
    171258
    
    MTK-Commit-Id: 1bc020d61a5ed2169dbdcff71d97e20f46e360fc
    
    CR-Id: ALPS09561468
    Feature: IMG GPU DDK driver
    Change-Id: I979f8923ca0427eb285cef4301bafb6e1ec39bb5

--- gpu_rgx/m24.2ED6620798/services/server/env/linux/pvr_gputrace.c
+++ gpu_rgx/m24.2ED6620798/services/server/env/linux/pvr_gputrace.c
@@ -80,6 +80,16 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endif
 #endif
 
+/* The Android kernel common has removed the tracing_on symbol. Ftrace are
+ * managed by the clients. DDK ensures the PVR trace functions remain enabled
+ * while features are available.
+ */
+#if 1
+#define TRACE_ON()
+#else
+#define TRACE_ON() tracing_on()
+#endif
+
 /******************************************************************************
  Module internal implementation
 ******************************************************************************/
@@ -444,14 +454,12 @@ void PVRGpuTraceInitIfEnabled(PVRSRV_DEVICE_NODE *psDeviceNode)
 			bEnable = IMG_TRUE;
 		}
 #endif
-#if defined(DEBUG) || !defined(ANDROID)
 		if (bEnable)
 		{
 			/* this enables FTrace globally (if not enabled nothing will appear
 			 * in the FTrace buffer) */
-			tracing_on();
+			TRACE_ON();
 		}
-#endif
 	}
 }
 
@@ -1361,14 +1369,12 @@ static PVRSRV_ERROR _GpuTraceSetEnabledCallback(
 			        pszOperation));
 		}
 #endif
-#if defined(DEBUG) || !defined(ANDROID)
 		if (value)
 		{
 			/* this enables FTrace globally (if not enabled nothing will appear
 			 * in the FTrace buffer) */
-			tracing_on();
+			TRACE_ON();
 		}
-#endif
 		/*  The HWPerf supplier is activated here,
 		    The FTrace consumer is activated above,
 		    The consumer should be active before the supplier    */
