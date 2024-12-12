5cd19d98e1defea3a765e73a7e2b6bd34e27cd96
mtk16965 <holiday.cheng@mediatek.com>
2024-12-12 20:40:48 +0800

    [ALPS09476089] r52p0: Add CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER
    [Description]
    1. Add CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER
    
    [Test]
    Basic
    
    MTK-Commit-Id: cab08e01361ab509b753587965eb80c8808e2338
    
    CR-Id: ALPS09476089
    Feature: ARM GPU DDK driver
    Change-Id: I20868a4a914c3db0bd8015b833de0e35d8ee0e91

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
@@ -46,6 +46,10 @@ static struct proc_dir_entry *proc_root;
 #include <platform/mtk_platform_common/mtk_platform_whitebox_directly_hard_reset.h>
 #endif /* CONFIG_MALI_MTK_WHITEBOX_DIRECTLY_HARD_RESET */
 
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER)
+#include <platform/mtk_platform_common/mtk_platform_whitebox_fault_worker.h>
+#endif /* CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER */
+
 static bool mfg_powered;
 static DEFINE_MUTEX(mfg_pm_lock);
 static DEFINE_MUTEX(common_debug_lock);
@@ -373,6 +377,9 @@ void mtk_common_debugfs_init(struct kbase_device *kbdev)
 #if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_DIRECTLY_HARD_RESET)
 	mtk_whitebox_directly_hard_reset_debugfs_init(kbdev);
 #endif /* CONFIG_MALI_MTK_WHITEBOX_DIRECTLY_HARD_RESET */
+#if IS_ENABLED(CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER)
+	mtk_whitebox_fault_worker_debugfs_init(kbdev);
+#endif /* CONFIG_MALI_MTK_WHITEBOX_FAULT_WORKER */
 
 	mtk_debug_sleep_mode_debugfs_init(kbdev);
 }
