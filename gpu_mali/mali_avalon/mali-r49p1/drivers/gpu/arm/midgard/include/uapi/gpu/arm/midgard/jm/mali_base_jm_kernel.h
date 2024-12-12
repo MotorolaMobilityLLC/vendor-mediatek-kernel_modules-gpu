c2528711a4b65a6c18a17a6ceb9065997beca4cd
Dongwei Gao <dongwei.gao@mediatek.com>
2024-12-12 20:20:35 +0800

    [ALPS08686646] GPU QoS porting (JM part) for kernel ddk migration to r49p1
    [Description]
    GPU QoS porting (JM part) for kernel ddk migration to r49p1
    
    [Test]
    Local build pass
    
    MTK-Commit-Id: cfcbad780ecae45a1a8b1377e787497498369ee1
    
    CR-Id: ALPS08686646
    Feature: GPU QoS Driver
    Change-Id: I8cd647ccbb83deb9cc28063bd7944abcca6227f3

--- gpu_mali/mali_avalon/mali-r49p1/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
+++ gpu_mali/mali_avalon/mali-r49p1/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
@@ -644,6 +644,9 @@ struct base_jd_atom_v2 {
 	base_jd_core_req core_req;
 	__u8 renderpass_id;
 	__u8 padding[7];
+#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+	u32 frame_nr;  /* frame number to the atom */
+#endif
 };
 
 /**
@@ -692,6 +695,9 @@ typedef struct base_jd_atom {
 	base_jd_core_req core_req;
 	__u8 renderpass_id;
 	__u8 padding[7];
+#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+	u32 frame_nr;  /* frame number to the atom */
+#endif
 } base_jd_atom;
 
 /* Job chain event code bits
