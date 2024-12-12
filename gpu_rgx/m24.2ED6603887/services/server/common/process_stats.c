dd20d21972a657bc44bcf043c87f83abf58f1cd0
akansha.shukla <akansha.shukla@mediatek.com>
2024-12-12 20:31:41 +0800

    [ALPS08771572] GPU: Update mt6761 to DDK 24.2ED
    [Description]
    Modify to use DDK 24.2 for mt6761
    [ALPS09170580] Remove extra parameter in Pvr memtrack_stats
    
    [Test]
    Boot pass, video playback pass, camera test pass
    
    MTK-Commit-Id: 2475f9bb6b4b23a0c3b2c0a388ca99c633106241
    
    Change-Id: Ibf5349a40a2db743cab62870a870a934a2ea3b68
    CR-Id: ALPS08771572
    Feature: IMG GPU DDK driver

--- gpu_rgx/m24.2ED6603887/services/server/common/process_stats.c
+++ gpu_rgx/m24.2ED6603887/services/server/common/process_stats.c
@@ -2444,14 +2444,13 @@ int RawProcessStatsPrintElements(OSDI_IMPL_ENTRY *psEntry, void *pvData)
 	DLLIST_NODE *psNode, *psNext;
 
 	DIPrintf(psEntry,
-	         "%s,%s,%s,%s,%s,%s,%s\n",
+	         "%s,%s,%s,%s,%s,%s\n",
 	         "PID",
 	         "MemoryUsageKMalloc",           // PVRSRV_PROCESS_STAT_TYPE_KMALLOC
 	         "MemoryUsageAllocPTMemoryUMA",  // PVRSRV_PROCESS_STAT_TYPE_ALLOC_PAGES_PT_UMA
 	         "MemoryUsageAllocPTMemoryLMA",  // PVRSRV_PROCESS_STAT_TYPE_ALLOC_PAGES_PT_LMA
 	         "MemoryUsageAllocGPUMemLMA",    // PVRSRV_PROCESS_STAT_TYPE_ALLOC_LMA_PAGES
-	         "MemoryUsageAllocGPUMemUMA",    // PVRSRV_PROCESS_STAT_TYPE_ALLOC_UMA_PAGES
-	         "MemoryUsageDmaBufImport");     // PVRSRV_PROCESS_STAT_TYPE_DMA_BUF_IMPORT
+	         "MemoryUsageAllocGPUMemUMA");    // PVRSRV_PROCESS_STAT_TYPE_ALLOC_UMA_PAGES
 
 	OSLockAcquire(g_psLinkedListLock);
 
@@ -2463,14 +2462,13 @@ int RawProcessStatsPrintElements(OSDI_IMPL_ENTRY *psEntry, void *pvData)
 			DIPrintf(psEntry,
 			         "%d,%"IMG_INT64_FMTSPECd",%"IMG_INT64_FMTSPECd","
 			         "%"IMG_INT64_FMTSPECd",%"IMG_INT64_FMTSPECd","
-			         "%"IMG_INT64_FMTSPECd",%"IMG_INT64_FMTSPECd"\n",
+			         "%"IMG_INT64_FMTSPECd"\n",
 			         psProcessStats->pid,
 			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_KMALLOC],
 			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_ALLOC_PAGES_PT_UMA],
 			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_ALLOC_PAGES_PT_LMA],
 			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_ALLOC_LMA_PAGES],
-			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_ALLOC_UMA_PAGES],
-			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_DMA_BUF_IMPORT]);
+			         psProcessStats->i64StatValue[PVRSRV_PROCESS_STAT_TYPE_ALLOC_UMA_PAGES]);
 		}
 	}
 
