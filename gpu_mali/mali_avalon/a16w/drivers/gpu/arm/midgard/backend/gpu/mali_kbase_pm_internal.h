9af8de78985e545654cec8f91e0acdb3f7598eb0
Knight Weng <knight.weng@mediatek.com>
2025-01-18 23:53:53 +0800

    [ALPS09691809] Fix autosuspend setting not working
    [Description]
     Fix autosuspend setting not working.
    
    [Test]
    Boot pass/UT/IT
    
    MTK-Commit-Id: c5fb4bcf8bcdd68dc9c09e02fe4bc6e5885b97f6
    
    CR-Id: ALPS09691809
    Feature: ARM GPU DDK driver
    Signed-off-by: Knight Weng <knight.weng@mediatek.com>
    Change-Id: I8787b588be8bc11efb98f3557591c86e813138a9

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_internal.h
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_internal.h
@@ -1050,6 +1050,11 @@ static inline bool kbase_pm_fw_sleep_on_idle_allowed(struct kbase_device *kbdev)
 	if (unlikely(kbdev->dev->power.autosuspend_delay <= 0))
 		return false;
 
+#if IS_ENABLED(CONFIG_MALI_MTK_ADAPTIVE_POWER_POLICY)
+	if (unlikely((int) ged_get_apo_autosuspend_delay_ms() == 0))
+		return false;
+#endif
+
 	return kbdev->pm.backend.gpu_sleep_allowed == KBASE_GPU_FW_SLEEP_ON_IDLE_ALLOWED;
 }
 
