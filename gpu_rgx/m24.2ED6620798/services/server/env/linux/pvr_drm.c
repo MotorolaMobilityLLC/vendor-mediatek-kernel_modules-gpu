0219e84cb6d6b919de8e3b193f9197857a63c081
sourav.sardana <sourav.sardana@mediatek.com>
2025-02-11 13:43:01 +0800

    [ALPS09586319] Adding fops handling
    [Description]
    Design change for k612
    
    [Test]
    Build pass
    
    MTK-Commit-Id: 22026072143e961a9c8d6f8d16c4452c29d300dd
    
    Feature: IMG GPU DDK driver
    CR-Id: ALPS09586319
    Change-Id: Ic18488da3ab400a4e41a7fbdc267994859c72099
    Signed-off-by: sourav.sardana <sourav.sardana@mediatek.com>

--- gpu_rgx/m24.2ED6620798/services/server/env/linux/pvr_drm.c
+++ gpu_rgx/m24.2ED6620798/services/server/env/linux/pvr_drm.c
@@ -518,6 +518,10 @@ const struct file_operations pvr_drm_fops = {
 #if defined(SUPPORT_LINUX_FDINFO)
 	.show_fdinfo	= pvr_show_fdinfo,
 #endif /* SUPPORT_LINUX_FDINFO */
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
+	.fop_flags		= FOP_UNSIGNED_OFFSET,
+#endif
 };
 
 const struct drm_driver pvr_drm_generic_driver = {
