25f73a60357e5ae73e3554dec4ee37fde1716311
jimmy-cl.wu <ds_jimmy-cl.wu@mediatek.com>
2025-02-19 00:43:16 +0800

    [ALPS09743988] [c7729]Race causes UAF(CVE-2025-0427)
    [Description]
    Arm Ticket: 03497308
    Arm Patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/7729
    
    GPUCORE-45584 Fix UAF issue with race between ctx term & PF worker
    
    During context termination, the driver would flush all fault workers to
    ensure that no other threads would access the contexts address space.
    However, if there are pending MMU faults then flushing might still return before all faults have been handled.
    
    [Test]
    CTS, MBTF Monkey
    
    MTK-Commit-Id: 51d9641417317e4e35c0e17cb0ec3197d5c15624
    
    CR-Id: ALPS09743988
    Feature: ARM GPU DDK driver
    Change-Id: I780f37f09e568c62961f8e89d070f89c51ff4614
    (cherry-pick from 51c029d0b2212632d8a33beeabbabb1c0838ee12)
    
    Change-Id: Ic178de08f5f296e4e0672e3c0b9e8cdad3b455da

--- gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf.c
+++ gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf.c
@@ -22,6 +22,7 @@
 #include <mali_kbase.h>
 #include <gpu/mali_kbase_gpu_fault.h>
 #include <mali_kbase_reset_gpu.h>
+#include <mali_kbase_ctx_sched.h>
 #include "mali_kbase_csf.h"
 #include "backend/gpu/mali_kbase_pm_internal.h"
 #include <linux/export.h>
@@ -1838,8 +1839,6 @@ void kbase_csf_ctx_handle_fault(struct kbase_context *kctx,
 void kbase_csf_ctx_term(struct kbase_context *kctx)
 {
 	struct kbase_device *kbdev = kctx->kbdev;
-	struct kbase_as *as = NULL;
-	unsigned long flags;
 	u32 i;
 	int err;
 	bool reset_prevented = false;
@@ -1890,15 +1889,57 @@ void kbase_csf_ctx_term(struct kbase_context *kctx)
 	flush_work(&kctx->kbdev->csf.fw_error_work);
 
 	/* A work item to handle page_fault/bus_fault/gpu_fault could be
-	 * pending for the outgoing context. Flush the workqueue that will
-	 * execute that work item.
+	 * pending for the outgoing context which we no longer care about.
+	 * Ensure that the context won't be accessed anymore by the fault
+	 * workers.
 	 */
-	spin_lock_irqsave(&kctx->kbdev->hwaccess_lock, flags);
-	if (kctx->as_nr != KBASEP_AS_NR_INVALID)
-		as = &kctx->kbdev->as[kctx->as_nr];
-	spin_unlock_irqrestore(&kctx->kbdev->hwaccess_lock, flags);
-	if (as)
-		flush_workqueue(as->pf_wq);
+	while (true) {
+		unsigned long flags;
+		int refcount;
+
+		mutex_lock(&kbdev->mmu_hw_mutex);
+		spin_lock_irqsave(&kbdev->hwaccess_lock, flags);
+
+		refcount = atomic_read(&kctx->refcount);
+		if ((refcount != 0) && !WARN_ON_ONCE(kctx->as_nr == KBASEP_AS_NR_INVALID)) {
+			struct kbase_as *as = &kctx->kbdev->as[kctx->as_nr];
+			int new_refcount;
+
+			dev_dbg(kbdev->dev,
+				"Waiting for pending fault worker to complete when terminating context (%d_%d)",
+				kctx->tgid, kctx->id);
+			spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
+			mutex_unlock(&kbdev->mmu_hw_mutex);
+			flush_workqueue(as->pf_wq);
+
+			new_refcount = atomic_read(&kctx->refcount);
+			if (refcount != new_refcount) {
+				/* Fault workers executed and released some references, re-check */
+				continue;
+			} else {
+				/* Waiting for pending fault workers to execute was not effective,
+				 * we're going to forcefully de-assign the AS from this context
+				 * because nothing else should still be accessing the context at
+				 * this point.
+				 *
+				 * This should never happen and a WARN_ON() would be printed by
+				 * kbase_ctx_sched_remove_ctx() if the refcount is non-zero.
+				 */
+				dev_warn(
+					kbdev->dev,
+					"No fault workers executed, %d refs remain for terminating context (%d_%d)",
+					new_refcount, kctx->tgid, kctx->id);
+				kbase_ctx_sched_remove_ctx(kctx);
+			}
+		} else {
+			kbase_ctx_sched_remove_ctx_nolock(kctx);
+
+			spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
+			mutex_unlock(&kbdev->mmu_hw_mutex);
+		}
+
+		break;
+	}
 
 	mutex_lock(&kctx->csf.lock);
 
