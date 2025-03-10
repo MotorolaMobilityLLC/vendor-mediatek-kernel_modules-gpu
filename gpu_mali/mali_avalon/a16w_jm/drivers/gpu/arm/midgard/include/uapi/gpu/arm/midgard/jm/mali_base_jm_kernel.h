23a9526f1a25eef7d9159982535d59a86938dcf5
Kuldeep Chaudhary <kuldeep.chaudhary@mediatek.com>
2025-03-10 15:41:34 +0800

    [ALPS09796576] GPU : mt6789 r54 KM
    [Description]
    1. mali-r49p1 --> a16w_jm
    2. Disable BRINGUP
    3. Enable QoS
    
    [Test]
    Boot pass, GPU running
    
    MTK-Commit-Id: 67f4a56f77100e83f05801d81a2088eadc4f49f7
    
    Feature: ARM GPU DDK driver
    CR-Id: ALPS09796576
    Change-Id: Ic4013986e85db8ede602d0f4db77e66e60bb0323
    Signed-off-by: kuldeep.chaudhary <kuldeep.chaudhary@mediatek.com>

--- gpu_mali/mali_avalon/a16w_jm/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
+++ gpu_mali/mali_avalon/a16w_jm/drivers/gpu/arm/midgard/include/uapi/gpu/arm/midgard/jm/mali_base_jm_kernel.h
@@ -543,6 +543,9 @@ struct base_jd_atom_v2 {
 	__u8 jobslot;
 	base_jd_core_req core_req;
 	__u8 padding[8];
+#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+	u32 frame_nr;  /* frame number to the atom */
+#endif
 };
 
 /**
@@ -589,6 +592,9 @@ typedef struct base_jd_atom {
 	base_jd_core_req core_req;
 	__u8 renderpass_id;
 	__u8 padding[7];
+#if defined(CONFIG_MALI_MTK_GPU_BM_JM)
+	u32 frame_nr;  /* frame number to the atom */
+#endif
 } base_jd_atom;
 
 /* Job chain event code bits
