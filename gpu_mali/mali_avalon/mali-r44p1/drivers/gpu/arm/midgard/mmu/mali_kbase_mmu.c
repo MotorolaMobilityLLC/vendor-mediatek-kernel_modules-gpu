3c7cbbcb33fa476d5acf3e135c761c73336ea189
jimmy-cl.wu <ds_jimmy-cl.wu@mediatek.com>
2025-03-19 00:43:42 +0800

    [ALPS09839069] [c7889]UAF of page metadata (CVE-2025-0819)
    [Description]
    Arm Ticket: 03500909
    Arm Patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/7894
    
    GPUCORE-45689 fix page-isolation/page-free race
    
    Prior to this change, the kbase_free_pages_worker would unlock each
    to-be-freed page before freeing the page metadata associated with it,
    which is performed by kbase_free_page_metadata. A page isolation by
    the kernel, in another thread, could thus perform a page isolation
    operation at the same time as the metadata was being freed, causing
    a user-after-free. This patch therefore extends the period in which
    the page is locked until the metadata is freed.
    
    [Test]
    CTS, MBTF Monkey
    
    MTK-Commit-Id: 0504d7710c4f936f612301ca8766aebf0b1938d8
    
    CR-Id: ALPS09839069
    Feature: ARM GPU DDK driver
    Change-Id: I1dcf7d1b72cda378d0d9ca94ba6da606eda939c1
    (cherry-pick from 718bedcc636debc7e0a9138fe6fb0a44d55f0b85)
    
    Change-Id: I7b0a031f34929be32c2c4abb1cd07c12263084a1

--- gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
+++ gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
 /*
  *
- * (C) COPYRIGHT 2010-2024 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2010-2025 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
@@ -3784,6 +3784,8 @@ static void kbase_mmu_mark_non_movable(struct page *page)
 	if (!kbase_is_page_migration_enabled())
 		return;
 
+	lock_page(page);
+
 	page_md = kbase_page_private(page);
 
 	spin_lock(&page_md->migrate_lock);
@@ -3792,7 +3794,9 @@ static void kbase_mmu_mark_non_movable(struct page *page)
 	if (IS_PAGE_MOVABLE(page_md->status))
 		page_md->status = PAGE_MOVABLE_CLEAR(page_md->status);
 
+	__ClearPageMovable(page);
 	spin_unlock(&page_md->migrate_lock);
+	unlock_page(page);
 }
 
 int kbase_mmu_init(struct kbase_device *const kbdev,
