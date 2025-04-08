dffe84b2b298359f434e4b2d0f7d165fd0100f7d
Garfield.Lin <garfield.lin@mediatek.com>
2025-04-08 22:49:46 +0800

    [ALPS09802077] [c7967] Fix incorrect sched state if RT suspend is aborted
    [Description]
    arm patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/7967
    Fix incorrect sched state if RT suspend is aborted
    
    [Test]
    GPU power on/off test pass
    
    MTK-Commit-Id: 1613576686ddf4caefade72e5020a4bd91d58550
    
    CR-Id: ALPS09802077
    Feature: ARM GPU DDK driver
    Change-Id: Ib500cdc7677d8f0a2eb9490edbc14f161efbed79

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
@@ -6586,8 +6586,14 @@ bool kbase_csf_scheduler_finalize_gpu_suspend(struct kbase_device *kbdev)
 {
 	struct kbase_csf_scheduler *const scheduler = &kbdev->csf.scheduler;
 
-	scheduler->state = SCHED_SUSPENDED;
-	KBASE_KTRACE_ADD(kbdev, SCHED_SUSPENDED, NULL, scheduler->state);
+	if (is_gpu_level_suspend_supported(kbdev)) {
+		/* Only transition to SCHED_SUSPENDED with GLS because
+		 * scheduler would have transitioned to this state already if
+		 * all CSGs were suspended successfully.
+		 */
+		scheduler->state = SCHED_SUSPENDED;
+		KBASE_KTRACE_ADD(kbdev, SCHED_SUSPENDED, NULL, scheduler->state);
+	}
 
 	wake_up_all(&kbdev->csf.event_wait);
 
@@ -8679,6 +8685,10 @@ int kbase_csf_scheduler_handle_runtime_suspend(struct kbase_device *kbdev)
 		return ret;
 	}
 
+	if (!is_gpu_level_suspend_supported(kbdev)) {
+		scheduler->state = SCHED_SUSPENDED;
+		KBASE_KTRACE_ADD(kbdev, SCHED_SUSPENDED, NULL, scheduler->state);
+	}
 #if IS_ENABLED(CONFIG_MALI_TRACE_POWER_GPU_WORK_PERIOD)
 	hrtimer_cancel(&scheduler->gpu_metrics_timer);
 #endif
