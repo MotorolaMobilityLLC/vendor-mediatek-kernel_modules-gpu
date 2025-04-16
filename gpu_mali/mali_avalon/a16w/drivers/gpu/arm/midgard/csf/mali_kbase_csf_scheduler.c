afcdad8020b6e97754a0f6191509c3f6064a3070
Wendell Lin <wendell.lin@mediatek.com>
2025-04-16 10:58:17 +0800

    [ALPS09585153] add mcu db check for power policy
    [Description]
    add mcu db check
    [Test]
    build pass
    
    MTK-Commit-Id: 4048a67eb3ac807be3df22b04a98ee513235795b
    
    CR-Id: ALPS09585153
    Feature: ARM GPU DDK driver
    Change-Id: I77db0746dcc7f09c9a5ea00fd9a7b394f0eaa72a
    Signed-off-by: Wendell Lin <wendell.lin@mediatek.com>

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
@@ -735,6 +735,14 @@ static enum hrtimer_restart apo_idle_timer_callback(struct hrtimer *timer)
 
 	return HRTIMER_NORESTART;
 }
+
+static bool mcu_db_is_disabled(struct kbase_device *kbdev)
+{
+	u32 db_notif_disabled = kbase_reg_read32(kbdev, GPU_CONTROL_ENUM(MCU_CONTROL)) &
+		MCU_CNTRL_DOORBELL_DISABLE_MASK;
+
+	return db_notif_disabled;
+}
 #endif /* CONFIG_MALI_MTK_ADAPTIVE_POWER_POLICY */
 
 static void release_doorbell(struct kbase_device *kbdev, int doorbell_nr)
@@ -933,7 +941,8 @@ void kbase_csf_scheduler_process_gpu_idle_event(struct kbase_device *kbdev)
 		 */
 #if IS_ENABLED(CONFIG_MALI_MTK_ADAPTIVE_POWER_POLICY)
 		/* Bypass enqueue */
-		if (kbdev->csf.scheduler.apo_support &&
+		if (!mcu_db_is_disabled(kbdev) &&
+			kbdev->csf.scheduler.apo_support &&
 			kbdev->csf.scheduler.state != SCHED_SLEEPING &&
 			ged_gpu_apo_notify()) {
 #if IS_ENABLED(CONFIG_MALI_MTK_DISABLE_SOI)
