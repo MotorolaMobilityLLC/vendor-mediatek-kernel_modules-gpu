5505945838c6e8e5c60ef9f00d5bcf13e13e1f71
Garfield.Lin <garfield.lin@mediatek.com>
2025-04-16 10:57:48 +0800

    [ALPS09911674] [c7983] fix MCU sleep wait condition in SYNC_UPDATE
    [Description]
    arm patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/7983
    
    MIDCET-6076 Correct MCU sleep wait condition in SYNC_UPDATE worker
    
    This commit updates the MCU sleep wait condition to verify that the MCU
    doorbell is disabled, ensuring the MCU is entering the SLEEP mode.
    Previously, the condition relied solely on the exit SLEEP flag and PM
    refcount, causing timeouts when the MCU did not transition to SLEEP.
    
    [Test]
    Test pass
    
    MTK-Commit-Id: f4f0e5867e5b36c1dc41c0ea06a4c938e88c3c6c
    
    CR-Id: ALPS09911674
    Feature: ARM GPU DDK driver
    Change-Id: Id443e3bb171e8aab43b52f2348d02be42e41390b

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
@@ -7410,6 +7410,7 @@ static void wait_for_mcu_sleep_before_sync_update_check(struct kbase_device *kbd
 	long timeout = kbase_csf_timeout_in_jiffies(kbdev->csf.csg_suspend_timeout_ms);
 	bool can_wait_for_mcu_sleep;
 	unsigned long flags;
+	bool db_notif_disabled;
 
 	lockdep_assert_held(&kbdev->csf.scheduler.lock);
 
@@ -7423,8 +7424,13 @@ static void wait_for_mcu_sleep_before_sync_update_check(struct kbase_device *kbd
 	 * Also there is no need to wait if the PM refcount is not zero,
 	 * which implies that MCU needs to be turned on.
 	 */
-	can_wait_for_mcu_sleep = !kbdev->pm.backend.exit_gpu_sleep_mode && !kbdev->pm.active_count;
+	db_notif_disabled = kbase_reg_read32(kbdev, GPU_CONTROL_ENUM(MCU_CONTROL)) &
+								 MCU_CNTRL_DOORBELL_DISABLE_MASK;
+	can_wait_for_mcu_sleep = !kbdev->pm.backend.exit_gpu_sleep_mode &&
+								 !kbdev->pm.active_count &&
+								 db_notif_disabled;
 	spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
+
 	if (!can_wait_for_mcu_sleep)
 		return;
 
