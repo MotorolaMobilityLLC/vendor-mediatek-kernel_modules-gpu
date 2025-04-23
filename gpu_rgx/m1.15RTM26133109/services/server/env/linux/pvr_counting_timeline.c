6d7e22356c2083919033d0f7d7c8dc04aeeecc35
chuan <ot_jchuan.jiang@mediatek.com>
2025-04-23 17:29:41 +0800

    Fix KE in pvr_counting_fence_timeline_debug_request
    Moto-CRs-Fixed: (CR)
    
    [Description]
    Fix timeline race condition
    In pvr_counting_fence_timeline_debug() there was a race condition if the debug callback was called half way through pvr_counting_fence_timeline_create().
    172042
    
    [Test]
    free text
    
    MTK-Commit-Id: 54bb1db560023eb47e5144c9c400943c149d1c16
    
    CR-Id: ALPS09945394
    Feature: IMG GPU DDK driver
    Change-Id: I434d7ce02ef25967268925ed1f742b5b7f707f24
    (cherry picked from commit 9e8d729caa51f2a5fe177a2dab2f41d5ddad2d40)
    Reviewed-on: https://gerrit.mot.com/3408069
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chuwei Xu <xucw@motorola.com>
    Submit-Approved: Jira Key

--- gpu_rgx/m1.15RTM26133109/services/server/env/linux/pvr_counting_timeline.c
+++ gpu_rgx/m1.15RTM26133109/services/server/env/linux/pvr_counting_timeline.c
@@ -146,6 +146,12 @@ struct pvr_counting_fence_timeline *pvr_counting_fence_timeline_create(
 	if (!timeline->context)
 		goto err_free_timeline;
 
+	timeline->current_value = 0;
+	timeline->next_value = 1;
+	kref_init(&timeline->kref);
+	spin_lock_init(&timeline->active_fences_lock);
+	INIT_LIST_HEAD(&timeline->active_fences);
+
 	srv_err = PVRSRVRegisterDbgRequestNotify(&timeline->dbg_request_handle,
 				dev_cookie,
 				pvr_counting_fence_timeline_debug_request,
@@ -156,12 +162,6 @@ struct pvr_counting_fence_timeline *pvr_counting_fence_timeline_create(
 		       __func__, PVRSRVGetErrorString(srv_err));
 		goto err_free_timeline_ctx;
 	}
-
-	timeline->current_value = 0;
-	timeline->next_value = 1;
-	kref_init(&timeline->kref);
-	spin_lock_init(&timeline->active_fences_lock);
-	INIT_LIST_HEAD(&timeline->active_fences);
 
 err_out:
 	return timeline;
