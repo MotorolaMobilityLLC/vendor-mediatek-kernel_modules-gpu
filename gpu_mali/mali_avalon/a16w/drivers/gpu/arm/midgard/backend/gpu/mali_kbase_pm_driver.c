4567b4ff897ec4a7376b31ed6c0a71470cd4d323
Wendell Lin <wendell.lin@mediatek.com>
2025-05-16 12:19:34 +0800

    [ALPS09980819] remove kbase workaround
    [Description]
    remove kbase workaround
    [Test]
    build pass
    
    MTK-Commit-Id: db797e46c07451815bfa060d5c5a511b3c6ae10d
    
    Signed-off-by: Wendell Lin <wendell.lin@mediatek.com>
    CR-Id: ALPS09980819
    Feature: ARM GPU DDK driver
    Change-Id: Ie800bd1d7348fbaf90c64cddbed8cbd513f36cea
    (cherry picked from commit 7951e10c4b663f65daab3df0da5826d8d530c190)

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_driver.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_driver.c
@@ -1830,8 +1830,7 @@ static int kbase_pm_mcu_update_state(struct kbase_device *kbdev)
 				kbase_hwcnt_backend_csf_on_after_mcu_off(&kbdev->hwcnt_gpu_iface);
 				backend->mcu_state = KBASE_MCU_IN_SLEEP;
 				kbase_pm_enable_db_mirror_interrupt(kbdev);
-				if (!atomic_read(&kbdev->csf.scheduler.fw_soi_enabled))
-					kbase_csf_scheduler_reval_idleness_post_sleep(kbdev);
+				kbase_csf_scheduler_reval_idleness_post_sleep(kbdev);
 				/* Enable PM interrupt, after MCU has been put
 				 * to sleep, for the power down of L2.
 				 */
