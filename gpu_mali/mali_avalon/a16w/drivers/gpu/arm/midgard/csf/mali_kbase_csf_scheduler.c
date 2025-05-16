72a1266862466d065a4478f3b76b70f813e58f12
Wendell Lin <wendell.lin@mediatek.com>
2025-05-16 12:19:34 +0800

    [ALPS09980819] fix state async issue
    [Description]
    fix async
    [Test]
    build pass
    
    MTK-Commit-Id: 3409e3a8d5dbac3d6d37e8620187ae65bf30b72d
    
    Signed-off-by: Wendell Lin <wendell.lin@mediatek.com>
    CR-Id: ALPS09980819
    Feature: ARM GPU DDK driver
    Change-Id: I2a03326738f973d90f244c7f04ad0025a70651b7
    (cherry picked from commit 86b79a26b8fec173827b02c5edb39aba0297ce74)

--- gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
+++ gpu_mali/mali_avalon/a16w/drivers/gpu/arm/midgard/csf/mali_kbase_csf_scheduler.c
@@ -912,9 +912,8 @@ void kbase_csf_scheduler_process_gpu_idle_event(struct kbase_device *kbdev)
 	struct kbase_csf_scheduler *scheduler = &kbdev->csf.scheduler;
 	bool can_suspend_on_idle;
 #if IS_ENABLED(CONFIG_MALI_MTK_ADAPTIVE_POWER_POLICY)
-	bool predict_apo;
 	ktime_t expiry_time;
-	u32 db_notif_disabled = kbase_reg_read32(kbdev, GPU_CONTROL_ENUM(MCU_CONTROL)) &
+	bool db_notif_disabled = kbase_reg_read32(kbdev, GPU_CONTROL_ENUM(MCU_CONTROL)) &
 			    MCU_CNTRL_DOORBELL_DISABLE_MASK;
 #endif /* CONFIG_MALI_MTK_ADAPTIVE_POWER_POLICY */
 
@@ -946,14 +945,17 @@ void kbase_csf_scheduler_process_gpu_idle_event(struct kbase_device *kbdev)
 #if IS_ENABLED(CONFIG_MALI_MTK_DISABLE_SOI)
 				kbase_pm_enable_db_mirror_interrupt(kbdev);
 #endif
-				predict_apo = ged_gpu_predict_apo_notify();
 
-				if (db_notif_disabled && !predict_apo) {
+				if (!ged_gpu_predict_apo_notify()) {
 #if IS_ENABLED(CONFIG_MALI_MTK_DISABLE_SOI)
 					kbase_pm_disable_db_mirror_interrupt(kbdev);
 #endif
 					enqueue_gpu_idle_work(scheduler);
 				} else {
+					if (db_notif_disabled) {
+						kbase_csf_ring_doorbell(kbdev, CSF_KERNEL_DOORBELL_NR);
+					}
+
 					if (!hrtimer_active(&scheduler->apo_idle_timer)) {
 						expiry_time = HR_TIMER_DELAY_NSEC(
 							ged_get_apo_wakeup_ns());
