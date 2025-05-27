8e79776f2c1c28af613766002ea78cb127c34894
akansha.shukla <akansha.shukla@mediatek.com>
2025-05-27 11:05:19 +0800

    [ALPS10049304] Add wk20 security patch
    [Description]
    PSP-83 : GPU DDK - GPU debug file gpu_mem_area leaks kernel VAs in the MEMDESCs information
    An unprivileged user can obtain kernel information by reading memory statistics generated from the GPU driver. On Android this is possible via the file path /proc/proc_stats/live_pids_stats/gpu_mem_area and /proc/proc_stats
    /retired_pids_stats/gpu_mem_area.
    Parts of this file leak kernel virtual memory addresses of the device on which it is obtained.
    The DDK kernel module has been updated to prevent the leak of sensitive kernel information in the gpu_mem_area .
    The is achieved using the kernels %pK modifier when displaying virtual addresses.
    
    [Test]
    deqp pass
    
    MTK-Commit-Id: e67524bd4719f95e5673d0ead0dcc447e9603305
    
    Change-Id: Ie65fe6ccef81c99bf312f8cd046b31abd2a0ee89
    CR-Id: ALPS10049304
    Feature: IMG GPU DDK driver
    Signed-off-by: akansha.shukla <akansha.shukla@mediatek.com>

--- gpu_rgx/m24.2ED6620798/services/server/common/ri_server.c
+++ gpu_rgx/m24.2ED6620798/services/server/common/ri_server.c
@@ -142,7 +142,7 @@ static IMG_CHAR g_szSysAllocImport[RI_SYS_ALLOC_IMPORT_FRMT_SIZE];
 #define RI_MEMDESC_ENTRY_IMPORT_FRMT     "{Import from PID %d}"
 #define RI_MEMDESC_ENTRY_IMPORT_BUF_SIZE (sizeof(RI_MEMDESC_ENTRY_IMPORT_FRMT)+5)
 
-#define RI_MEMDESC_ENTRY_FRMT      "%%sPID:%%-5d DEV:%%s 0x%%010" IMG_UINT64_FMTSPECx "\t%%-%ds %%s\t0x%%010" IMG_UINT64_FMTSPECx "\t<%%p> %%s%%s %%s%%c"
+#define RI_MEMDESC_ENTRY_FRMT      "%%sPID:%%-5d DEV:%%s 0x%%010" IMG_UINT64_FMTSPECx "\t%%-%ds %%s\t0x%%010" IMG_UINT64_FMTSPECx "\t<%%016" IMG_UINT64_FMTSPECx "> %%s%%s %%s%%c"
 #define RI_MEMDESC_ENTRY_BUF_SIZE  (sizeof(RI_MEMDESC_ENTRY_FRMT)+(3+5+RI_DEV_ID_BUF_SIZE+10+PVR_ANNOTATION_MAX_LEN+RI_MEMDESC_ENTRY_PROC_BUF_SIZE+16+\
                                                RI_MEMDESC_ENTRY_IMPORT_BUF_SIZE+RI_SYS_ALLOC_IMPORT_FRMT_SIZE)) + sizeof(RI_FREED_BY_DRIVER)
 #define RI_MEMDESC_ENTRY_FRMT_SIZE (sizeof(RI_MEMDESC_ENTRY_FRMT))
@@ -1844,7 +1844,7 @@ static void _GenerateMEMDESCEntryString(RI_SUBLIST_ENTRY *psRISubEntry,
 				   pszAnnotationText,
 				   (bDebugFs ? "" : (char *)szProc),
 				   psRISubEntry->ui64Size,
-				   psRIPMR,
+				   psRISubEntry->psRI != NULL ? GET_SERIALNUM(psRISubEntry->psRI) : 0,
 				   (IS_IMPORT(psRISubEntry) ? (char *)&szImport : ""),
 				   (!IS_IMPORT(psRISubEntry) &&
 				    (bSysPMR) &&
